b8a3417c8b6db2f42d22cb11f78b22eb4e51fb0a
noahgiboney <noahgiboney@gmail.com>
2024-01-21 13:30:45 -0800

    feat: sidebar logic, loop through some sidebar options

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,8 +1,32 @@
+"use client"
+import { useSelectedLayoutSegment } from "next/navigation"
+import Link from "next/link"
 
 const Sidebar = () => {
+
+    const sideBarOptions = [
+        {name: "Dashboard", href: "/admin"},
+        {name: "Events", href: "/events"}
+    ]
+
     return (
         <div>
-            
+            <div>
+                <h2>Admin</h2>
+            </div>
+            <div>
+                <ul>
+                    {sideBarOptions.map((option) => (
+                        <li key={option.name}>
+                            <Link href={option.href}></Link>
+                            {option.name}
+                        </li>
+                    ))
+                    }
+                </ul>
+            </div>
         </div>
     )
 }
+
+export default Sidebar
