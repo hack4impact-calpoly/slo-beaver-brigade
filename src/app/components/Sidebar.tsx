541d36684c56c6c3533a1c580d4d7e528a37df23
noahgiboney <noahgiboney@gmail.com>
2024-01-21 14:13:02 -0800

    feat: basic styling setup

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,35 +1,34 @@
-"use client"
-import { useSelectedLayoutSegment } from "next/navigation"
-import Link from "next/link"
+"use client";
+import { useSelectedLayoutSegment } from "next/navigation";
+import Link from "next/link";
+import style from "@styles/adminStyle/sidebar.module.css";
 
 const Sidebar = () => {
+  // returns the current route that user is on, null if user is on the root (admin page.tsx)
+  const segement = useSelectedLayoutSegment();
 
-    // returns the current route that user is on, null if user is on the root (admin page.tsx)
-    const segement = useSelectedLayoutSegment()
-    
-    const sideBarOptions = [
-        {name: "Dashboard", href: "/admin", current: !segement ? true : false},
-        {name: "Events", href: "/events", current: `/${segement}` === "/events" ? true: false}
-    ]
+  // can add more options here for admin
+  const sideBarOptions = [
+    { name: "Dashboard", href: "/admin", current: !segement ? true : false },
+    { name: "Events", href: "/admin/events", current: `/${segement}` === "/events" ? true : false},
+  ];
 
-    return (
-        <div>
-            <div>
-                <h2>Admin</h2>
-            </div>
-            <div>
-                <ul>
-                    {sideBarOptions.map((option) => (
-                        <li key={option.name}>
-                            <Link href={option.href}></Link>
-                            {option.name}
-                        </li>
-                    ))
-                    }
-                </ul>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={style.sidebar}>
+      <div>
+        <h2>Admin</h2>
+      </div>
+      <div className={style.adminOptions}>
+        <ul>
+          {sideBarOptions.map((option) => (
+            <li key={option.name}>
+              <Link href={option.href}>{option.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
