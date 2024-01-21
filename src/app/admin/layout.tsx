ba2b9e46dc277fb897edccc006eb5f49d24322b0
noahgiboney <noahgiboney@gmail.com>
2024-01-21 14:59:05 -0800

    update: small styling structuring

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,23 +1,20 @@
 import { ReactNode } from "react";
 import Sidebar from "@components/Sidebar";
-import style from '@styles/adminStyle/layout.module.css'
+import style from "@styles/adminStyle/layout.module.css";
 
 type Props = {
-    children: ReactNode;
-}
+  children: ReactNode;
+};
 
 const layout = (props: Props) => {
-    return (
-      <div className={style.adminDash}>
-        <div>
-            <Sidebar/>
-        </div>
-        <main className={style.mainContainer}>
-            {props.children}
-        </main>
+  return (
+    <div className={style.adminDash}>
+      <div>
+        <Sidebar />
       </div>
-    );
-  };
-  
-  export default layout;
-  
+      <main className={style.mainContainer}>{props.children}</main>
+    </div>
+  );
+};
+
+export default layout;
