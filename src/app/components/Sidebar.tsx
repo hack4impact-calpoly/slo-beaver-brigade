6efc18aa359c7fdc216f8bce9a3de31b4817be4f
noahgiboney <noahgiboney@gmail.com>
2024-01-21 16:12:00 -0800

    feat: sidebar collapsable and expandable using usestate

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,31 +1,47 @@
 "use client";
 import { useSelectedLayoutSegment } from "next/navigation";
+import { useState } from "react";
 import Link from "next/link";
 import style from "@styles/adminStyle/sidebar.module.css";
+import { ArrowsPointingInIcon, ArrowsPointingOutIcon, HomeIcon, CalendarDaysIcon } from "@heroicons/react/16/solid";
 
 const Sidebar = () => {
   // returns the current route that user is on, null if user is on the root (admin page.tsx)
   const segement = useSelectedLayoutSegment();
 
+  // state for determining if slider is collapased
+  const [isCollapsed, setCollapsed] = useState(false);
+
   // can add more options here for admin
   const sideBarOptions = [
-    { name: "Dashboard", href: "/admin", current: !segement ? true : false },
+    { name: "Dashboard", href: "/admin", icon: <HomeIcon style={{ width: '35px', height: '35px' }}/> ,current: !segement ? true : false },
     {
       name: "Events",
       href: "/admin/events",
+      icon: <CalendarDaysIcon style={{ width: '35px', height: '35px' }}/>,
       current: `/${segement}` === "/events" ? true : false,
     },
   ];
 
+  const toggleSlider = () => {
+    setCollapsed(!isCollapsed)
+  }
+
   return (
-    <div className={style.sidebar}>
-      <h1>Admin</h1>
+    <div className={`${style.sidebar} ${isCollapsed ? style.collapsedSidebar : ''}`}>
+      <div className={style.toggleButton} onClick={toggleSlider}>
+        {isCollapsed ?  <ArrowsPointingOutIcon style={{ width: '35px', height: '35px' }}/> : <ArrowsPointingInIcon style={{ width: '35px', height: '35px' }}/> }
+      </div>
+      <h1>{isCollapsed ? '' : 'Admin'}</h1>
       <div className={style.adminOptions}>
         <ul>
           {sideBarOptions.map((option) => (
             <li key={option.name}>
               <Link href={option.href}>
-                <p>{option.name}</p>
+                <div className={style.option}>
+                {option.icon}
+                <p>{isCollapsed ? '' : option.name}</p>
+                </div>
               </Link>
             </li>
           ))}
