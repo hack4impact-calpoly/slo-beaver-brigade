eba81891364dcedd972c71c5c597d4a6555b1f6b
noahgiboney <noahgiboney@gmail.com>
2024-01-21 22:42:03 -0800

    feat: mobile responsivness

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import { Love_Ya_Like_A_Sister, Montserrat } from "next/font/google";
 import Sidebar from "@components/Sidebar";
 import style from "@styles/adminStyle/layout.module.css";
 
@@ -6,13 +7,17 @@ type Props = {
   children: ReactNode;
 };
 
+const montserrat = Montserrat({ subsets: ["latin"], weight: ["300"] });
+
 const layout = (props: Props) => {
   return (
-    <div className={style.adminDash}>
-      <div>
-        <Sidebar />
+    <div className={montserrat.className}>
+      <div className={style.adminDash}>
+        <div>
+          <Sidebar />
+        </div>
+        <main className={style.mainContainer}>{props.children}</main>
       </div>
-      <main className={style.mainContainer}>{props.children}</main>
     </div>
   );
 };
