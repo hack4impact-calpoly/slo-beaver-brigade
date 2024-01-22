5ac44178f689f8aa4a2f2af6cd8007c020a95da3
noahgiboney <noahgiboney@gmail.com>
2024-01-21 22:42:03 -0800

    feat: mobile responsivness

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -3,44 +3,63 @@ import { useSelectedLayoutSegment } from "next/navigation";
 import { useState } from "react";
 import Link from "next/link";
 import style from "@styles/adminStyle/sidebar.module.css";
-import { ArrowsPointingInIcon, ArrowsPointingOutIcon, HomeIcon, CalendarDaysIcon } from "@heroicons/react/16/solid";
+import {
+  Bars3Icon,
+  XMarkIcon,
+  HomeIcon,
+  CalendarDaysIcon,
+} from "@heroicons/react/16/solid";
 
 const Sidebar = () => {
   // returns the current route that user is on, null if user is on the root (admin page.tsx)
   const segement = useSelectedLayoutSegment();
 
-  // state for determining if slider is collapased
+  // useState for determining if slider is collapased
   const [isCollapsed, setCollapsed] = useState(false);
 
   // can add more options here for admin
   const sideBarOptions = [
-    { name: "Dashboard", href: "/admin", icon: <HomeIcon style={{ width: '35px', height: '35px' }}/> ,current: !segement ? true : false },
+    {
+      name: "Dashboard",
+      href: "/admin",
+      icon: <HomeIcon style={{ width: "35px", height: "35px" }} />,
+      current: !segement ? true : false,
+    },
     {
       name: "Events",
       href: "/admin/events",
-      icon: <CalendarDaysIcon style={{ width: '35px', height: '35px' }}/>,
+      icon: <CalendarDaysIcon style={{ width: "35px", height: "35px" }} />,
       current: `/${segement}` === "/events" ? true : false,
     },
   ];
 
   const toggleSlider = () => {
-    setCollapsed(!isCollapsed)
-  }
+    setCollapsed(!isCollapsed);
+  };
 
   return (
-    <div className={`${style.sidebar} ${isCollapsed ? style.collapsedSidebar : ''}`}>
+    <div
+      className={`${style.sidebar} ${isCollapsed ? style.collapsedSidebar : ""}`}
+    >
       <div className={style.toggleButton} onClick={toggleSlider}>
-        {isCollapsed ?  <ArrowsPointingOutIcon style={{ width: '35px', height: '35px' }}/> : <ArrowsPointingInIcon style={{ width: '35px', height: '35px' }}/> }
+        {isCollapsed ? (
+          <Bars3Icon style={{ width: "35px", height: "35px" }} />
+        ) : (
+          <XMarkIcon style={{ width: "35px", height: "35px" }} />
+        )}
       </div>
-      <h1>{isCollapsed ? '' : 'Admin'}</h1>
+      <h1>{isCollapsed ? "" : "Admin"}</h1>
       <div className={style.adminOptions}>
         <ul>
           {sideBarOptions.map((option) => (
-            <li key={option.name}>
+            <li
+              key={option.name}
+              className={`${option.current ? style.currentOption : ""}`}
+            >
               <Link href={option.href}>
                 <div className={style.option}>
-                {option.icon}
-                <p>{isCollapsed ? '' : option.name}</p>
+                  {option.icon}
+                  <p>{isCollapsed ? "" : option.name}</p>
                 </div>
               </Link>
             </li>
