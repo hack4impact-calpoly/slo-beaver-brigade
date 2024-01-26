6c46d7de80c73e461c61b9b9dc32daf26e06f646
Logan Costello <logancostello2022@gmail.com>
2024-01-25 17:35:01 -0800

    fix: debugging modal import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@styles/Button";
+import CreateEvent from "@components/CreateEvent";
+
 export default function Home() {
   return (
     <main>
       <h1>SLO Beaver Brigade</h1>
       <Button>Nice Button</Button>
+      <CreateEvent></CreateEvent>
     </main>
   );
 }
