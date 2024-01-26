abec4fdd043c065866c1d3758b5c1b11cd6fa489
Logan Costello <logancostello2022@gmail.com>
2024-01-25 17:35:01 -0800

    fix: debugging modal import

---
+++ src/app/components/CreateEvent.tsx
@@ -0,0 +1,13 @@
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+  } from '@chakra-ui/react'
+
+export default function CreateEvent() {
+    return <div>This is a create event component</div>;
+  }
