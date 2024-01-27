a152c832af0863aab8144e59c7c461679f151305
HanYu Wu <hanyuwu04@gmail.com>
2024-01-27 12:24:25 -0800

    added chakra ui modal

--- src/app/components/ExpandedViewComponent.tsx
+++ src/app/components/ExpandedViewComponent.tsx
@@ -1,6 +1,19 @@
 // src/app/components/EventExpandedView.tsx
+"use client";
 import React, { useContext } from 'react';
-import { Button } from '@styles/Button';
+import {
+    Button,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalCloseButton,
+    ModalBody,
+    ModalFooter,
+    useDisclosure,
+    Box,
+  } from '@chakra-ui/react';
+  
 
 interface EventExpandedViewProps {
     eventDetails: {
@@ -13,22 +26,44 @@ interface EventExpandedViewProps {
 }
 
 const EventExpandedView: React.FC<EventExpandedViewProps> = ({eventDetails}) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  // needed the authentication to be implemented first
   //const { isAdmin } = useContext(AuthContext); // Update based on your actual auth context
 
+  const isAdmin = true;
+
   return (
-    <div>
+    <center>
+    <Box p={4} borderWidth="1px" borderRadius="lg">
       <h2>{eventDetails.name}</h2>
       <p>{eventDetails.description}</p>
-      <Button>Sign Up</Button>
+      <Button onClick={onOpen}>Sign Up</Button>
 
       {/* Conditional Edit/Delete Buttons */}
-      {true && ( // isAdmin
+      {isAdmin && ( // isAdmin
         <>
           <Button>Edit</Button>
           <Button>Delete</Button>
         </>
       )}
-    </div>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{eventDetails.name}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <p>{eventDetails.description}</p>
+            {/* Additional modal content can be added here */}
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </Box>
+    </center>
   );
 };
 
