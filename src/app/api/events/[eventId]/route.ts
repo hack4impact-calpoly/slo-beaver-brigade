f507db7ba1a14bf8ad3aaeafa16f73aa7f5cfe93
Jason <Jason1@jelincic.com>
2024-01-27 14:35:22 -0800

    feat: creates api event routes and adds Get requests for all events and a single event

---
+++ src/app/api/events/[eventId]/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import connectDB from "@database/db";
+// import { Event } from "@models/event";
+
+type IParams = {
+  params: {
+    eventName: string
+  }
+}
+
+export async function GET(req: NextRequest, { params }: IParams) {
+  await connectDB() // function from db.ts before
+  const { eventName } = params // another destructure
+
+   try {
+        const event = await Event.findOne({ eventName }).orFail()
+        return NextResponse.json(event)
+    } catch (err) {
+        return NextResponse.json('Blog not found: ' + {eventName} , { status: 404 })
+    }
+}
