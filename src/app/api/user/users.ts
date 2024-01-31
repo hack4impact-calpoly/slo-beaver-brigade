ec86456821ca638f1fade62b822873ea4768f3c7
noahgiboney <noahgiboney@gmail.com>
2024-01-30 21:39:56 -0800

    feat: put request to add user to the db

--- src/app/api/user/users.ts
+++ src/app/api/user/users.ts
@@ -1,18 +1,32 @@
 import connectDB from "@database/db";
 import { NextResponse } from "next/server";
-import User  from '@database/userSchema'
-import userSchema from "@database/userSchema";
+import User, { IUser } from "@database/userSchema";
 
+// get endpoint to return array of all users
 /**
  * @returns {allUsers: User[]}
  */
 export async function GET() {
-    try {
-        await connectDB();
-        const allUsers = await User.find();
-        return NextResponse.json({ allUsers });
-    } catch(error) { 
-        console.error("error fetching the users", error)
-    }
+  try {
+    await connectDB();
+    const allUsers = await User.find();
+    return NextResponse.json({ allUsers });
+  } catch (error) {
+    console.error("error fetching the users", error);
+  }
 }
 
+// put endpoint to upload user to mongoDB
+/**
+ * @param {object} data
+ * @return {object}
+ */
+export async function PUT(data: IUser) {
+  try {
+    await connectDB();
+    const newUser = await User.create(data);
+    return NextResponse.json({ newUser });
+  } catch (error) {
+    console.error("error adding user", error);
+  }
+}
--- src/database/userSchema.ts
+++ src/database/userSchema.ts
@@ -1,5 +1,10 @@
 import mongoose, { Schema } from "mongoose";
 
+export interface IUser {
+  email: string;
+  password: string;
+}
+
 //! Example user schema. Not guaranteed to work
 const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
