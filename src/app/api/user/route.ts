18bbc6e993a71877a37605e6efa4dc30a04c9e75
noahgiboney <noahgiboney@gmail.com>
2024-01-31 21:46:53 -0800

    feat: post request for adding user working

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,13 +1,60 @@
 import connectDB from "@database/db";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import User, { IUser } from "@database/userSchema";
 
+// GER request for getting all users
 export async function GET() {
-  await connectDB();
   try {
+    await connectDB();
     const users = await User.find({});
     return NextResponse.json({ users });
   } catch (error) {
-    return NextResponse.json({ error: (error as Error).message }, { status: 500 });
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 }
+    );
   }
 }
+
+// POST request for adding user
+export async function POST(req: NextRequest) {
+    try {
+      await connectDB();
+  
+      // convert the readable stream to JSON
+      const bodyText = await new Response(req.body).text();
+      const body = JSON.parse(bodyText);
+      const { email, password } = body;
+  
+      // make sure user has username and password
+      if (!email || !password) {
+        return NextResponse.json(
+          { error: 'Email and password are required' },
+          { status: 400 }
+        );
+      }
+      
+      // check if user already exsists
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return NextResponse.json(
+          { error: 'User already exists' },
+          { status: 409 }
+        );
+      }
+  
+      const newUser = new User({ email, password });
+      await newUser.save();
+  
+      return NextResponse.json(
+        { message: 'User created successfully', user: newUser },
+        { status: 201 }
+      );
+    } catch (error) {
+      return NextResponse.json(
+        { error: (error as Error).message },
+        { status: 500 }
+      );
+    }
+  }
+  
