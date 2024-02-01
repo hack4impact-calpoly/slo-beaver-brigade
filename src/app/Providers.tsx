2b36f131bcf005a3b1fa81da626a189a49500e09
javalosr2004 <jesusavalosr2004@gmail.com>
2024-01-31 19:25:14 -0800

    revamp: moved ChakraProvider to Providers.tsx

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,10 +1,15 @@
 "use client";
+import { ChakraProvider } from "@chakra-ui/react";
 import StyledComponentsRegistry from "./lib/registry";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
+    
     <StyledComponentsRegistry>
-        {children}
+        <ChakraProvider>
+            {children}
+        </ChakraProvider>
     </StyledComponentsRegistry>
+    
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@
 
 import { Button } from "@styles/Button";
 import { ChakraProvider } from '@chakra-ui/react'
+import CreateEditEvent from "@components/CreateEditEvent";
 
 export default function Home() {
   return (
-    <ChakraProvider>
       <main>
         <h1>SLO Beaver Brigade</h1>
         <Button>Nice Button</Button>
+        <CreateEditEvent create={true}></CreateEditEvent>
       </main>    
-    </ChakraProvider>
   );
 }
