034a8fde711737997e2b558c86b119153839d78d
Ella Hagen <elhagen@calpoly.edu>
2024-02-03 11:50:36 -0800

    feat: added success screen

--- src/app/components/CreateAccount.tsx
+++ src/app/components/CreateAccount.tsx
@@ -1,26 +1,60 @@
+"use client";
 import styles from './CreateAccount.module.css'
 import React, { useState } from 'react';
+import checkmarkImage from '/docs/images/checkmark.png'
+import Image from 'next/image'
+
 export default function CreateAccount() {
-    //const [isOverlayVisible, setIsOverlayVisible] = useState(true);
+    const [isSubmitted, setSubmitted] = useState(false);
+
+    const handleSubmit = () => {
+        // If the form submission is successful, setSubmitted(true);
+        // This should also handle the backend submission later.
+        //for the frontend, it'll just have the successful submission screen popup
+        setSubmitted(true);
+    };
+
+
 
     return (
-        <div className={styles.main}>
-            <div className = {styles.topColor}/>
-            <div className={styles.formBody}>
-                <h1>
-                    Create Account
-                </h1>
-                <form className={styles.form}>
-                    <input className={styles.input} type="text" id="name" placeholder="Name"/>
-                    <input className={styles.input} type="text" id="email" placeholder="Email"/>
-                    <input className={styles.input} type="password" id="password" placeholder="Password"/>
-                    <input className={styles.halfInput} type="tel" id="phone" placeholder="Phone"/>
-                    <input className={styles.halfInput} type="text" id="zipcode" placeholder="Zipcode"/>
-                    <textarea className={styles.input} id="questions" placeholder="Interest Questions"/>
-                    <button className={styles.button} type="button" id = "submit">Create Account</button>
-                </form>   
-            </div>  
+        <div className={styles.background}>
+            <div className={styles.main}>
+                <div className = {styles.topColor}/>
+                <div className={styles.formBody}>
+                    <h1>
+                        Create Account
+                    </h1>
+                    <form className={styles.form}>
+                        <input className={styles.input} type="text" id="name" placeholder="Name"/>
+                        <input className={styles.input} type="text" id="email" placeholder="Email"/>
+                        <input className={styles.input} type="password" id="password" placeholder="Password"/>
+                        <input className={styles.halfInput} type="tel" id="phone" placeholder="Phone"/>
+                        <input className={styles.halfInput} type="text" id="zipcode" placeholder="Zipcode"/>
+                        <textarea className={styles.input} id="questions" placeholder="Interest Questions"/>
+                        <button className={styles.button} type="button" id = "submit" onClick={handleSubmit}>
+                            Create Account</button>
+                    </form>   
+                </div>  
+            </div>
+            <div>
+                {isSubmitted && <AccountCreated/>}
+            </div>
         </div>
     )
   }
-  
+
+ //When return to calendar is clicked it should reroute to the calendar
+const AccountCreated = () => {
+    return(
+        <div className = {styles.successWindow}>
+            <div className={styles.totalAccountCreated}>
+                <Image className={styles.checkmark} src={checkmarkImage} alt="checkmark"/>
+                <div className={styles.successText}>SUCCESS</div>
+                <div className={styles.successExplanation}>Your account has been created</div>
+                <div className={styles.buttonContainer}>
+                    <button className={styles.returnToCalendar}>Return to calendar</button>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,6 @@ import { Button } from "@styles/Button";
 import CreateAccount from "@components/CreateAccount"
 export default function Home() {
   return (
-    <main>
-      <h1>SLO Beaver Brigade</h1>
-      <Button>Nice Button</Button>
-    </main>
+      <CreateAccount></CreateAccount>
   );
 }
