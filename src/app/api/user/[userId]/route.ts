8f0fde4a9b08f49a8332fbee48a990abb887491f
noahgiboney <noahgiboney@gmail.com>
2024-02-03 14:52:05 -0800

    feat: fixed files with new clone of repo, ditched the forked version

---
+++ src/app/api/user/[userId]/route.ts
@@ -0,0 +1,32 @@
+import connectDB from "@database/db";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@database/userSchema";
+
+// dynamic GET request to get user by ID
+export async function GET(req: NextRequest) {
+  try {
+    await connectDB();
+
+    // extract id from the URL
+    const url = new URL(req.url);
+    const pathSegments = url.pathname.split("/");
+    const userIDIndex =
+      pathSegments.findIndex((segment) => segment === "user") + 1;
+    const userID = pathSegments[userIDIndex];
+
+    const user = await User.findById(userID);
+
+    // Check if user exists
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ user });
+  } catch (error) {
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
+
