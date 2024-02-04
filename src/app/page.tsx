0154d1d7abbfd42f3bd508c1a96c3eddc796c24a
Sohini-pillay <sohinispillay@gmail.com>
2024-02-04 13:33:54 -0800

    fixed link to login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@styles/Button";
 import { ChakraProvider } from "@chakra-ui/react";
 import CreateEditEvent from "@components/CreateEditEvent";
-import Link from "next/link";
+import NextLink from "next/link";
 
 export default function Home() {
   return (
@@ -11,9 +11,7 @@ export default function Home() {
       <h1>SLO Beaver Brigade</h1>
       <Button>Nice Button</Button>
       <CreateEditEvent create={true}></CreateEditEvent>
-      <Link href="/login">
-        <Button>Login</Button>
-      </Link>
+      <NextLink href="/login">Login</NextLink>
     </main>
   );
 }
