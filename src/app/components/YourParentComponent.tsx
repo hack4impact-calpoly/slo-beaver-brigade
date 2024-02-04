e0500dd741218b230f71bcd1b1a70ffaae639cfb
HanYu Wu <hanyuwu04@gmail.com>
2024-02-04 00:51:13 -0800

    changed data type of date to Date, added startTime and endTime with Date as type

--- src/app/components/YourParentComponent.tsx
+++ src/app/components/YourParentComponent.tsx
@@ -3,10 +3,17 @@ import React from 'react';
 import EventExpandedView from './ExpandedViewComponent';
 
 const YourParentComponent: React.FC = () => {
+  const startTime = new Date();
+  startTime.setHours(8, 0, 0); // Set hours, minutes, and seconds for 8 am
+
+  const endTime = new Date();
+  endTime.setHours(12, 0, 0); // Set hours, minutes, and seconds for 12 pm
+
   const eventDetails = {
     name: 'Watery Walk',
-    date: 'January 10',
-    time: '8 AM - 9 AM',
+    date: new Date('2022-12-12'),
+    startTime: startTime,
+    endTime: endTime,
     location: 'East Main Street',
     description: 'This is a sample event description.',
     weekDay: 'Wednesday',
