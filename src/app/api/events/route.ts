ab9699bf29575da400effc07f3d9cbebfc6cfa20
Jason <Jason1@jelincic.com>
2024-02-04 21:04:26 -0800

    feat: updates API calls to properly use the eventId

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -9,6 +9,7 @@ export async function GET() {
 	try {
 			// query for all events and sort by date
 	    const events = await Event.find().sort({ date: -1 }).orFail()
+      console.log(events)
 			// returns all events in json format or errors
 	    return NextResponse.json(events, { status: 200 });
 	} catch (err) {
@@ -20,13 +21,13 @@ export async function POST(req: NextRequest) {
   await connectDB() // connect to db
 
   //strip Event data from req json
-  const { eventName, description, wheelchairAccessible, spanishSpeakingAccommodation, date, volunteerEvent, attendeeIds }: IEvent = await req.json()
+  const { eventName, description, wheelchairAccessible, spanishSpeakingAccommodation, startTime, endTime, volunteerEvent, groupsAllowed, attendeeIds }: IEvent = await req.json()
   
   //create new event or return error
   try {
-    const newEvent = new Event({ eventName, description, wheelchairAccessible, spanishSpeakingAccommodation, date, volunteerEvent, attendeeIds })
+    const newEvent = new Event({ eventName, description, wheelchairAccessible, spanishSpeakingAccommodation, startTime, endTime, volunteerEvent, groupsAllowed, attendeeIds })
     await newEvent.save()
-    return NextResponse.json("Event added successfully\n" + {newEvent}, { status: 200 });
+    return NextResponse.json("Event added successfull: " + newEvent, { status: 200 });
   } catch (err) {
     return NextResponse.json("Event not added: " + err, { status: 400 });
   }
