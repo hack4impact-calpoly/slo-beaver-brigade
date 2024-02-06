7b42967e63427757babcf40824d531113460c1b4
noahgiboney <noahgiboney@gmail.com>
2024-02-05 22:57:30 -0800

    feat: icon for close list

--- src/app/components/UsersAttendingList.tsx
+++ src/app/components/UsersAttendingList.tsx
@@ -1,6 +1,9 @@
 "use client"
 import React, { useState } from "react";
 import style from '@styles/admin/userList.module.css'
+import {
+  XMarkIcon
+} from "@heroicons/react/16/solid";
 
 export default function UserAttendingList() {
   const [list, setList] = useState(false);
@@ -18,19 +21,19 @@ export default function UserAttendingList() {
   return (
     <>
       <button onClick={toggleList} className={style.toggleButton}>
-        Attendies
+        Attendees
       </button>
 
       {list && (
         <div className={style.list}>
           <div onClick={toggleList} className={style.overlay}></div>
           <div className={style.listContent}>
-            <h2>Attendies</h2>
+            <h2>Attendees</h2>
             <p>
-              Ex
+              User list here
             </p>
             <button className={style.closeList} onClick={toggleList}>
-              CLOSE
+              <XMarkIcon style={{ width: "20px", height: "20px" }} />
             </button>
           </div>
         </div>
