3995f8effffd5c5e638b0c181da70bf23a57e093
javalosr2004 <jesusavalosr2004@gmail.com>
2024-02-06 14:35:10 -0800

    chore: updated route names

--- src/app/components/Sidebar_Guest.tsx
+++ src/app/components/Sidebar_Guest.tsx
@@ -27,25 +27,25 @@ const Sidebar = () => {
   const sideBarOptions = [
     {
       name: "Home",
-      href: "/guest",
+      href: "/dashboard",
       icon: <HomeIcon style={{ width: "35px", height: "35px" }} />,
       current: !segement ? true : false,
     },
     {
       name: "Events",
-      href: "/guest/events",
+      href: "/dashboard/events",
       icon: <CalendarDaysIcon style={{ width: "35px", height: "35px" }} />,
       current: `/${segement}` === "/events" ? true : false,
     },
     {
       name: "Volunteer",
-      href: "/guest/volunteer",
+      href: "/dashboard/volunteer",
       icon: <HandRaisedIcon style={{ width: "35px", height: "35px" }} />,
       current: `/${segement}` === "/volunteer" ? true : false, 
     },
     {
       name: "Profile",
-      href: "/guest/profile",
+      href: "/dashboard/profile",
       icon: <UserIcon style={{ width: "35px", height: "35px" }} />,
       current: `/${segement}` === "/profile" ? true : false, 
     },
