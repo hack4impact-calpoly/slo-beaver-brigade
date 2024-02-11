b262c996ecd4a52e3ea25176bcc0662dd44f4533
HanYu Wu <hanyuwu04@gmail.com>
2024-02-10 16:53:11 -0800

    added chakra provider

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 import StyledComponentsRegistry from "./lib/registry";
-
+import { ChakraProvider } from "@chakra-ui/react";
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     
