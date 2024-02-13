ebd0484fdc0d856df340ccd87d20acbcf6a5afe2
Logan Costello <logancostello2022@gmail.com>
2024-02-12 16:57:53 -0800

    feat: added time to create/edit components

--- src/app/components/CreateEvent.tsx
+++ src/app/components/CreateEvent.tsx
@@ -57,49 +57,30 @@ const CreateEvent = () => {
 
           <ModalBody>
             <Stack spacing={3}>
-              <Input
-                variant="flushed"
-                placeholder="Event Name"
-                fontWeight="bold"
-              />
-              <Input
-                variant="flushed"
-                placeholder="Event Location"
-                fontWeight="bold"
-              />
-              <Stack spacing={0}>
-                <FormLabel color="grey" fontWeight="bold">
-                  Event Date
-                </FormLabel>
-                <Input
-                  placeholder="Select Date"
-                  size="md"
-                  type="date"
-                  color="grey"
-                />
-              </Stack>
-              <Stack spacing={0}>
-                <FormLabel color="grey" fontWeight="bold">
-                  Event Description
-                </FormLabel>
-                <Textarea placeholder="Event Description" />
-              </Stack>
-              <Stack spacing={0}>
-                <FormLabel color="grey" fontWeight="bold">
-                  Event Type
-                </FormLabel>
-                <Select placeholder="Select option" color="grey">
-                  <option value="option1">Watery Walk</option>
-                  <option value="option2">Pond Clean Up</option>
-                  <option value="option3">Habitat Monitoring</option>
-                </Select>
-              </Stack>
-              <Switch fontWeight="bold" color="grey">
-                Wheelchair Accessibility
-              </Switch>
-              <Switch fontWeight="bold" color="grey">
-                Spanish Speaking Accommodation
-              </Switch>
+                <Input variant='flushed' placeholder='Event Name' fontWeight='bold'/>
+                <Input variant='flushed' placeholder='Event Location' fontWeight='bold'/>
+                <Stack spacing={0}>
+                    <FormLabel color='grey' fontWeight='bold'>Event Date</FormLabel>
+                    <Input placeholder="Select Date" size="md" type="date" color='grey'/>
+                </Stack>
+                <Stack spacing={0}>
+                    <FormLabel color='grey' fontWeight='bold'>Event Time</FormLabel>
+                    <Input placeholder="Select Time" size="md" type="time" color='grey'/>
+                </Stack>
+                <Stack spacing={0}>
+                    <FormLabel color='grey' fontWeight='bold'>Event Description</FormLabel>
+                    <Textarea placeholder='Event Description' />
+                </Stack>
+                <Stack spacing={0}>
+                    <FormLabel color='grey' fontWeight='bold'>Event Type</FormLabel>
+                    <Select placeholder='Select option' color='grey'>
+                        <option value='option1'>Watery Walk</option>
+                        <option value='option2'>Pond Clean Up</option>
+                        <option value='option3'>Habitat Monitoring</option>
+                    </Select>
+                </Stack>
+                <Switch fontWeight='bold' color='grey'>Wheelchair Accessibility</Switch>
+                <Switch fontWeight='bold' color='grey'>Spanish Speaking Accommodation</Switch>
             </Stack>
           </ModalBody>
 
