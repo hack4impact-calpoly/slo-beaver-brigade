d0e8cef2cbd4b81fe56b906216d8c64d78f7fe51
noahgiboney <noahgiboney@gmail.com>
2024-02-17 18:44:13 -0800

    feat: map over events fetched from endpoint with basic styling

--- src/app/dashboard/events/page.tsx
+++ src/app/dashboard/events/page.tsx
@@ -1,9 +1,59 @@
-const dashboard = () => {
+"use client"
+import React, { useEffect, useState } from 'react';
+import style from  '@styles/dashboard/events.module.css';
+
+interface IEvent {
+  _id: string;
+  eventName: string;
+  location: string;
+  description: string;
+  wheelchairAccessible: boolean;
+  spanishSpeakingAccommodation: boolean;
+  startTime: Date;
+  endTime: Date;
+  volunteerEvent: boolean;
+  groupsAllowed: string[]; 
+  attendeeIds: string[];
+}
+
+const Dashboard: React.FC = () => {
+  const [events, setEvents] = useState<IEvent[]>([]);
+
+  // get events from api endpoint
+  const fetchEvents = async () => {
+    try {
+      const response = await fetch('/api/events'); 
+      if (!response.ok) {
+        throw new Error('Failed to fetch events');
+      }
+      const data = await response.json();
+      setEvents(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // useEffect to fetch events on component mount
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
   return (
+    //map over events to list upcoming events
     <div>
       <h2>Event Guest Page</h2>
+      <ul className={style.eventsList}>
+        {events.map((event) => (
+          <li key={event._id} className={style.eventItem}>
+            <h3 className={style.eventTitle}>{event.eventName}</h3>
+            <p className={style.eventLocation}>Location: {event.location}</p>
+            <p className={style.eventDescription}>Description: {event.description}</p>
+            <p className={style.eventTime}>Start Time: {new Date(event.startTime).toLocaleString()}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
-export default dashboard;
+export default Dashboard;
