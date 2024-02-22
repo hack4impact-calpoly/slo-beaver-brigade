918f8b31dc3c490bfb810bfba5daa25b67c71fc1
HanYu Wu <hanyuwu04@gmail.com>
2024-02-21 16:41:36 -0800

    initilization of profile edit button and calendar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,21 @@
+"use client"
+import DashboardCalendar from "@components/DashboardCalendar";
+import profileImage from "../../profile.png";
+import Image from "next/image";
+import "./profile.css";
+
 const dashboard = () => {
+
   return (
     <div>
-      <h2>Main Guest Dashboard</h2>
+      <div className="profile-container">
+        <div className="profile-image-container">
+          <Image src={profileImage} alt="Profile" width={60} height={60} className="profile-image" />
+        </div>
+        <button className="edit-profile-button">Edit Profile</button>
+        <DashboardCalendar />
+      </div>
+      
     </div>
   );
 };
