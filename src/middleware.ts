ab3d05d47ee98e5bdfbda6ec81af5fe21bd2305c
sgill33 <sgill33@calpoly.edu>
2024-02-24 13:46:39 -0800

    removed calendar pages from public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,17 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: [
-        "/",
-        "/admin",
-        "/admin/events",
-        "/calendar",
-        "/dashboard",
-        "/login",
-        "/signup",
-        "/forgot-password",
-        "/reset-password",
-    ],
+  publicRoutes: [
+    "/",
+    "/admin",
+    "/dashboard",
+    "/login",
+    "/signup",
+    "/forgot-password",
+    "/reset-password",
+  ],
 });
 
 export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
