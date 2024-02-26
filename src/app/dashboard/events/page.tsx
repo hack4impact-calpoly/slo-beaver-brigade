6079f0f8a211997275150ca2019cc5fbcb4cff43
Sohini-pillay <sohinispillay@gmail.com>
2024-02-25 20:05:29 -0800

    create connected

--- src/app/dashboard/events/page.tsx
+++ src/app/dashboard/events/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+import CreateEvent from "@components/CreateEvent";
+
 const dashboard = () => {
   return (
     <div>
       <h2>Event Guest Page</h2>
+      <CreateEvent />
     </div>
   );
 };
