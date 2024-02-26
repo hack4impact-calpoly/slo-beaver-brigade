09ce1073dc49d254463536a0c42b9b292abcb145
noahgiboney <noahgiboney@gmail.com>
2024-02-26 14:13:38 -0800

    feat: profile picture for table and mobile responsive adjustments

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,7 +1,10 @@
 "use client";
-import { Table, Thead, Tbody, Tr, Th, Td, Box } from "@chakra-ui/react";
+import { Table, Thead, Tbody, Tr, Th, Td, Box, useBreakpointValue } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
-import style from "@styles/admin/events.module.css";
+import style from "@styles/admin/users.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import beaverLogo from '/docs/images/beaver-logo.svg';
 
 interface IUser {
   _id: string;
@@ -18,43 +21,69 @@ interface IUser {
 }
 
 const UserList = () => {
-  // states
-  const [users, setEvents] = useState<IUser[]>([]);
+  const [users, setUsers] = useState<IUser[]>([]);
+  const [sortOrder, setSortOrder] = useState("earliest");
+  const [searchTerm, setSearchTerm] = useState("");
 
-  // get all users from route
-  const fetchEvents = async () => {
+  const tablePadding = useBreakpointValue({ base: "0", md: "4" });
+
+  const fetchUsers = async () => {
     try {
       const response = await fetch("/api/user");
       if (!response.ok) {
         throw new Error("Failed to fetch users");
       }
       const data = await response.json();
-      console.log("Fetched data:", data);
-
-      const usersArray = data.users || [];
-      setEvents(usersArray);
+      setUsers(data.users || []);
     } catch (error) {
       console.error(error);
     }
   };
 
   useEffect(() => {
-    fetchEvents();
+    fetchUsers();
   }, []);
 
   return (
-    <Box className={style.mainContainer}>
-      <Table variant="simple">
-        <Tbody>
-          {users.map((user) => (
-            <Tr key={user._id}>
-              <Td>{`${user.firstName} ${user.lastName}`}</Td>
-              <Td>{user.email}</Td>
-            </Tr>
-          ))}
-        </Tbody>
-      </Table>
-    </Box>
+    <div className={style.mainContainer}>
+      <div className={style.tableContainer}>
+        <div className={style.buttonContainer}>
+          <select
+            value={sortOrder}
+            onChange={(e) => setSortOrder(e.target.value)}
+            className={style.sortSelect}
+          >
+            <option value="earliest">Earliest First</option>
+            <option value="latest">Latest First</option>
+          </select>
+          <input
+            type="text"
+            placeholder="Search for user"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className={style.searchBar}
+          />
+        </div>
+        <Box overflowX="auto">
+          <Table variant="striped" size="sm">
+            <Tbody>
+              {users.map((user) => (
+                <Tr key={user._id}>
+                  <Td><Image src={beaverLogo} alt="profile picture" width="50" height="30" style={{ minWidth: '50px' }}/></Td>
+                  <Td padding={tablePadding}>{`${user.firstName} ${user.lastName}`}</Td>
+                  <Td padding={tablePadding}>{user.email}</Td>
+                  <Td>
+                  <Link href={""} className={style.viewDetails}>
+                      View Details
+                    </Link>
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </Box>
+      </div>
+    </div>
   );
 };
 
