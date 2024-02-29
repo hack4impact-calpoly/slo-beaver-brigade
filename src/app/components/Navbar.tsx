547d2b508da84b364f0c004e8b361ef83f7bdfc7
sgill33 <sgill33@calpoly.edu>
2024-02-28 20:14:06 -0800

    redid navbar component without chakra

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,107 +1,51 @@
-"use client";
-import { useState } from "react";
-import {
-  useColorMode,
-  Switch,
-  Flex,
-  Button,
-  IconButton,
-} from "@chakra-ui/react";
-import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
-import NextLink from "next/link";
+"use client"
+import { useState } from 'react'
+import Link from 'next/link'
+import "@styles/navbar/navbar.css"
+import { HamburgerIcon, Search2Icon, StarIcon } from '@chakra-ui/icons'
 
-export const Navbar = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
-  const isDark = colorMode === "dark";
-  const [display, changeDisplay] = useState("none");
+
+const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false)
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
+  }
+
   return (
-    <div>
-      <Flex>
-        <Flex
-          position="fixed"
-          align="center"
-          backgroundColor={"brown"}
-          width={"100%"}
-          bottom={"1rem"}
-        >
-          {/* Desktop */}
-          <Flex display={["none", "none", "flex", "flex"]}>
-            <NextLink href="/" passHref>
-              <Button variant="ghost" aria-label="Home" my={5} w="100%">
-                Home
-              </Button>
-            </NextLink>
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">
+          <StarIcon></StarIcon>
+        </div>
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          <HamburgerIcon/>
+        </div>
+        <div className={`nav-elements  ${showNavbar && 'active'}`}>
+          <ul>
+            <li>
+              <Link href="/">Visit</Link>
+            </li>
+            <li>
+              <Link href="/">Volunteer</Link>
+            </li>
+            <li>
+              <Link href="/">Learn</Link>
+            </li>
+            <li>
+              <Link href="/">About Us</Link>
+            </li>
+            <li>
+              <Link href="/">Donate</Link>
+            </li>
+            <li>
+              <Link href="/"> <Search2Icon className='search-icon'/> </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
 
-            <NextLink href="/about" passHref>
-              <Button variant="ghost" aria-label="About" my={5} w="100%">
-                About
-              </Button>
-            </NextLink>
-
-            <NextLink href="/contact" passHref>
-              <Button variant="ghost" aria-label="Contact" my={5} w="100%">
-                Contact
-              </Button>
-            </NextLink>
-          </Flex>
-
-          {/* Mobile */}
-          <IconButton
-            aria-label="Open Menu"
-            size="lg"
-            mr={2}
-            icon={<HamburgerIcon />}
-            onClick={() => changeDisplay("flex")}
-            display={["flex", "flex", "none", "none"]}
-          />
-          <Switch color="green" isChecked={isDark} onChange={toggleColorMode} />
-        </Flex>
-
-        {/* Mobile Content */}
-        <Flex
-          w="100vw"
-          display={display}
-          bgColor="gray.50"
-          zIndex={20}
-          h="100vh"
-          pos="fixed"
-          top="0"
-          left="0"
-          overflowY="auto"
-          flexDir="column"
-        >
-          <Flex justify="flex-end">
-            <IconButton
-              mt={2}
-              mr={2}
-              aria-label="Open Menu"
-              size="lg"
-              icon={<CloseIcon />}
-              onClick={() => changeDisplay("none")}
-            />
-          </Flex>
-
-          <Flex flexDir="column" align="center">
-            <NextLink href="/" passHref>
-              <Button variant="ghost" aria-label="Home" my={5} w="100%">
-                Home
-              </Button>
-            </NextLink>
-
-            <NextLink href="/about" passHref>
-              <Button variant="ghost" aria-label="About" my={5} w="100%">
-                About
-              </Button>
-            </NextLink>
-
-            <NextLink href="/contact" passHref>
-              <Button variant="ghost" aria-label="Contact" my={5} w="100%">
-                Contact
-              </Button>
-            </NextLink>
-          </Flex>
-        </Flex>
-      </Flex>
-    </div>
-  );
-};
+export default Navbar
