5eb1a1f6ea92c7a9171533812c1e6a996e7ab69f
sgill33 <sgill33@calpoly.edu>
2024-03-01 16:06:21 -0800

    added parent component to navbar to pass user's name

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,17 +1,19 @@
-"use client"
-import { useState } from 'react'
-import Link from 'next/link'
-import styles from "@styles/navbar/navbar.module.css"
-import { HamburgerIcon, Search2Icon, StarIcon } from '@chakra-ui/icons'
-import { currentUser } from '@clerk/nextjs';
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import styles from "@styles/navbar/navbar.module.css";
+import { HamburgerIcon, Search2Icon, StarIcon } from "@chakra-ui/icons";
 
 
-const Navbar = () => {
-  const [showNavbar, setShowNavbar] = useState(false)
+
+
+export default function Navbar(props:{name:string}) {
+
+  const [showNavbar, setShowNavbar] = useState(false);
 
   const handleShowNavbar = () => {
-    setShowNavbar(!showNavbar)
-  }
+    setShowNavbar(!showNavbar);
+  };
 
   return (
     <nav className={styles.navbar}>
@@ -21,37 +23,50 @@ const Navbar = () => {
             <StarIcon></StarIcon>
           </Link>
           <div className={styles.greeting}>
-            <p>Sign In</p>
+            <p>{props.name}</p>
           </div>
         </div>
         <div className={styles.menu_icon} onClick={handleShowNavbar}>
-          <HamburgerIcon/>
+          <HamburgerIcon />
         </div>
-        <div className={`${styles.nav_elements}  ${showNavbar && styles.active}`}>
+        <div
+          className={`${styles.nav_elements}  ${showNavbar && styles.active}`}
+        >
           <ul>
             <li>
-              <Link href="https://www.slobeaverbrigade.com/come-to-a-beaver-pond/">Visit</Link>
+              <Link href="https://www.slobeaverbrigade.com/come-to-a-beaver-pond/">
+                Visit
+              </Link>
             </li>
             <li>
-              <Link href="https://www.slobeaverbrigade.com/news-events/">Volunteer</Link>
+              <Link href="https://www.slobeaverbrigade.com/news-events/">
+                Volunteer
+              </Link>
             </li>
             <li>
-              <Link href="https://www.slobeaverbrigade.com/block-content-examples/">Learn</Link>
+              <Link href="https://www.slobeaverbrigade.com/block-content-examples/">
+                Learn
+              </Link>
             </li>
             <li>
-              <Link href="https://www.slobeaverbrigade.com/about-us/">About Us</Link>
+              <Link href="https://www.slobeaverbrigade.com/about-us/">
+                About Us
+              </Link>
             </li>
             <li>
-              <Link href="https://www.slobeaverbrigade.com/donate/">Donate</Link>
+              <Link href="https://www.slobeaverbrigade.com/donate/">
+                Donate
+              </Link>
             </li>
             <li>
-              <Link href="/"> <Search2Icon className={styles.search_icon}/> </Link>
+              <Link href="/">
+                {" "}
+                <Search2Icon className={styles.search_icon} />{" "}
+              </Link>
             </li>
           </ul>
         </div>
       </div>
     </nav>
-  )
-}
-
-export default Navbar
+  );
+};
