2bec00529b3c2510d10d57af401755d34bf5e0f6
sgill33 <sgill33@calpoly.edu>
2024-03-02 11:18:41 -0800

    changed website font to lato

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,19 @@ import { ClerkProvider } from '@clerk/nextjs'
 import "./globals.css";
 import Providers from "./Providers";
 import NavbarParent from "@components/NavbarParents";
+import { Lato } from "next/font/google";
 
 //! Update metadata to match your project
 export const metadata: Metadata = {
   title: "SLO Beaver Brigade",
   description: "Created with love.",
 };
+
+const lato = Lato({
+  weight: '400',
+  subsets: ['latin'],
+})
+
 
 export default function RootLayout({
   children,
@@ -17,7 +24,7 @@ export default function RootLayout({
 }) {
   return (
       <html lang="en">
-        <body>
+        <body  className={lato.className}>
           <Providers>
               <NavbarParent admin={false}/>
               {children}
