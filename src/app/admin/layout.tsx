44d23fea6c6459a8ade759383c24f9a62df66ed7
sgill33 <sgill33@calpoly.edu>
2024-03-02 14:05:54 -0800

    removed navbar from admin layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,7 +2,6 @@ import { ReactNode } from "react";
 import { Montserrat } from "next/font/google";
 import Sidebar from "@components/Sidebar";
 import style from "@styles/admin/layout.module.css";
-import NavbarParent from "@components/NavbarParents";
 
 type Props = {
   children: ReactNode;
@@ -13,7 +12,6 @@ const montserrat = Montserrat({ subsets: ["latin"], weight: ["300"] });
 const Layout = (props: Props) => {
   return (
     <div className={montserrat.className}>
-      <NavbarParent admin={true}></NavbarParent>
       <div className={style.adminDash}>
         <div>
           <Sidebar />
