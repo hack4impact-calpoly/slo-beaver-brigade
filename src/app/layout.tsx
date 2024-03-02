68f22bf89801f01723d262a9fdee9826c37b4bb6
sgill33 <sgill33@calpoly.edu>
2024-03-01 16:39:19 -0800

    added navbar parent with props to base layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       <html lang="en">
         <body>
           <Providers>
-              <NavbarParent/>
+              <NavbarParent admin={false}/>
               {children}
           </Providers>
           </body>
