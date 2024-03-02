5baa60d265140e9984a3fb0ce20674b76b0aa275
sgill33 <sgill33@calpoly.edu>
2024-03-01 16:39:57 -0800

    added boolean to determine whether to load admin or user navbar

--- src/app/components/NavbarParents.tsx
+++ src/app/components/NavbarParents.tsx
@@ -1,12 +1,12 @@
-import React from "react"
-import Navbar from "./Navbar"
+import React from "react";
+import Navbar from "./Navbar";
 import { currentUser } from "@clerk/nextjs";
 
-export default async function NavbarParent(){
-    const user = await currentUser();
+export default async function NavbarParent(props:{admin:boolean}) {
+  const user = await currentUser();
 
-    if (!user) return <Navbar name="SignIn/SignUp"></Navbar>;
- 
-    const name = `Hi ${user?.firstName}!`
-    return <Navbar name={name} ></Navbar>;
+  if (!user) return <Navbar name="Sign In / Log In" admin={props.admin}></Navbar>;
+
+  const name = `Hi ${user?.firstName}!`;
+  return <Navbar name={name} admin={props.admin}></Navbar>;
 }
