b16241b1b32b672f0d29554e9a343c7e824294f6
sgill33 <sgill33@calpoly.edu>
2024-03-01 16:39:32 -0800

    added navbar parent with props to admin layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import { Montserrat } from "next/font/google";
 import Sidebar from "@components/Sidebar";
 import style from "@styles/admin/layout.module.css";
+import NavbarParent from "@components/NavbarParents";
 
 type Props = {
   children: ReactNode;
@@ -12,6 +13,7 @@ const montserrat = Montserrat({ subsets: ["latin"], weight: ["300"] });
 const Layout = (props: Props) => {
   return (
     <div className={montserrat.className}>
+      <NavbarParent admin={true}></NavbarParent>
       <div className={style.adminDash}>
         <div>
           <Sidebar />
