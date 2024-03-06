abdc0f8bb4fffd2e0b172c54cebf774d585447c4
Shuya Yanase <your-email@email.com>
2024-03-05 16:55:36 -0800

    fixed issues

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,8 +2,7 @@ import { ReactNode } from "react";
 import { Montserrat } from "next/font/google";
 import Sidebar from "@components/Sidebar";
 import style from "@styles/admin/layout.module.css";
-import TabBar from '../components/TabBar';
-
+import TabBar from "../components/TabBar";
 
 type Props = {
   children: ReactNode;
@@ -13,15 +12,16 @@ const montserrat = Montserrat({ subsets: ["latin"], weight: ["300"] });
 
 const Layout = (props: Props) => {
   return (
-    <div className={montserrat.className}>
-      <div className={style.adminDash}>
-        <div>
-          <TabBar />
+    <>
+      <TabBar />
+
+      <div className={montserrat.className}>
+        <div className={style.adminDash}>
           <Sidebar />
+          <main className={style.mainContainer}>{props.children}</main>
         </div>
-        <main className={style.mainContainer}>{props.children}</main>
       </div>
-    </div>
+    </>
   );
 };
 
