71e01a943772a5aaa2a55be442c77e54a66ea7ac
HanYu Wu <hanyuwu04@gmail.com>
2024-03-17 11:02:46 -0700

    adjusted create event button styling and format for better visual

--- src/app/createEvent/page.tsx
+++ src/app/createEvent/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useRef, useState } from "react";
 import {
   Box,
@@ -15,7 +15,7 @@ import {
   Flex,
   Select,
   Textarea,
-  IconButton
+  IconButton,
 } from "@chakra-ui/react";
 import { AddIcon, CalendarIcon } from "@chakra-ui/icons";
 import MiniCalendar from "../components/MiniCalendar";
@@ -31,7 +31,8 @@ const CreateEvent = () => {
   const [location, setLocation] = useState("");
   const [language, setLanguage] = useState("");
   const [description, setDescription] = useState("");
-  const [accessibilityAccommodation, setAccessibilityAccommodation] = useState("");
+  const [accessibilityAccommodation, setAccessibilityAccommodation] =
+    useState("");
   const [requiredItems, setRequiredItems] = useState("");
 
   const handleEventNameChange = (e) => setEventName(e.target.value);
@@ -56,7 +57,6 @@ const CreateEvent = () => {
       fileInputRef.current.click();
     }
   };
-
 
   const handleCreateEvent = async () => {
     if (!eventName) {
@@ -86,59 +86,57 @@ const CreateEvent = () => {
       </Text>
 
       <FormControl mb="4" onClick={promptFileInput} cursor="pointer">
-    <Input
-      id="cover-image"
-      type="file"
-      accept="image/*"
-      onChange={handleImageChange}
-      ref={fileInputRef}
-      hidden // Hide the actual input
-    />
-    <Box
-      position="relative"
-      borderWidth="1px"
-      p="4"
-      mt="4"
-      textAlign="center"
-      h="64"
-      borderRadius="20px"
-      overflow="hidden"
-      bg="gray.200"
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      flexDirection="column"
-    >
-      {!imagePreview ? (
-        <>
-          <Text>Upload Image</Text>
-          <IconButton
-            aria-label="Upload image"
-            icon={<AddIcon />}
-            mt="2"
-          />
-        </>
-      ) : (
-        <Image
-          src={imagePreview}
-          alt="Event cover preview"
-          position="absolute"
-          top={0}
-          left={0}
-          width="100%"
-          height="100%"
-          objectFit="fill"
-          zIndex={0}
+        <Input
+          id="cover-image"
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={fileInputRef}
+          hidden // Hide the actual input
         />
-      )}
-    </Box>
-  </FormControl>
+        <Box
+          position="relative"
+          borderWidth="1px"
+          p="4"
+          mt="4"
+          textAlign="center"
+          h="64"
+          borderRadius="20px"
+          overflow="hidden"
+          bg="gray.200"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+        >
+          {!imagePreview ? (
+            <>
+              <Text>Upload Image</Text>
+              <IconButton aria-label="Upload image" icon={<AddIcon />} mt="2" />
+            </>
+          ) : (
+            <Image
+              src={imagePreview}
+              alt="Event cover preview"
+              position="absolute"
+              top={0}
+              left={0}
+              width="100%"
+              height="100%"
+              objectFit="fill"
+              zIndex={0}
+            />
+          )}
+        </Box>
+      </FormControl>
 
-      <Flex direction={{ base: "column", md: "row" }} gap={20}>
+      <Flex direction={{ base: "column", md: "row" }} gap={20} mb={6}>
         <VStack spacing={4} align="stretch" flex="1">
           {/* All your form controls */}
           <FormControl isRequired>
-            <FormLabel htmlFor="event-name" fontWeight="bold">Event Name</FormLabel>
+            <FormLabel htmlFor="event-name" fontWeight="bold">
+              Event Name
+            </FormLabel>
             <Input
               id="event-name"
               placeholder="Enter event name"
@@ -159,8 +157,14 @@ const CreateEvent = () => {
 
           <Flex justifyContent="space-between" width="100%">
             <FormControl isRequired width="48%">
-              <FormLabel htmlFor="event-type" fontWeight="bold">Event Type</FormLabel>
-              <Select id="event-type" placeholder="Select" onChange={(e) => setEventType(e.target.value)}>
+              <FormLabel htmlFor="event-type" fontWeight="bold">
+                Event Type
+              </FormLabel>
+              <Select
+                id="event-type"
+                placeholder="Select"
+                onChange={(e) => setEventType(e.target.value)}
+              >
                 {/* Populate with options */}
                 <option value="Watery Walk">Watery Walk</option>
                 <option value="Volunteer">Volunteer</option>
@@ -168,8 +172,14 @@ const CreateEvent = () => {
             </FormControl>
 
             <FormControl isRequired width="48%">
-              <FormLabel htmlFor="organization" fontWeight="bold">Organization</FormLabel>
-              <Select id="organization-type" placeholder="Select" onChange={(e) => setOrganization(e.target.value)}>
+              <FormLabel htmlFor="organization" fontWeight="bold">
+                Organization
+              </FormLabel>
+              <Select
+                id="organization-type"
+                placeholder="Select"
+                onChange={(e) => setOrganization(e.target.value)}
+              >
                 {/* Populate with options */}
                 <option value="Watery Walk">I dont know</option>
                 <option value="Volunteer">I dont know</option>
@@ -177,45 +187,83 @@ const CreateEvent = () => {
             </FormControl>
           </Flex>
 
-
           <FormControl isRequired>
-            <FormLabel htmlFor="location" fontWeight="bold">Location</FormLabel>
-            <Input id="location" placeholder="Enter event location" onChange={(e) => setLocation(e.target.value)} />
+            <FormLabel htmlFor="location" fontWeight="bold">
+              Location
+            </FormLabel>
+            <Input
+              id="location"
+              placeholder="Enter event location"
+              onChange={(e) => setLocation(e.target.value)}
+            />
           </FormControl>
 
           <FormControl isRequired>
-            <FormLabel htmlFor="language" fontWeight="bold">Language</FormLabel>
-            <Select id="language-type" placeholder="Select" onChange={(e) => setLanguage(e.target.value)}>
-                {/* Populate with options */}
-                <option value="English">English</option>
-                <option value="Spanish">Spanish</option>
+            <FormLabel htmlFor="language" fontWeight="bold">
+              Language
+            </FormLabel>
+            <Select
+              id="language-type"
+              placeholder="Select"
+              onChange={(e) => setLanguage(e.target.value)}
+            >
+              {/* Populate with options */}
+              <option value="English">English</option>
+              <option value="Spanish">Spanish</option>
             </Select>
           </FormControl>
 
           <FormControl isRequired>
-            <FormLabel htmlFor="description" fontWeight="bold">Description</FormLabel>
-            <Textarea id="description" placeholder="Enter event description" onChange={(e) => setDescription(e.target.value)} />
+            <FormLabel htmlFor="description" fontWeight="bold">
+              Description
+            </FormLabel>
+            <Textarea
+              id="description"
+              placeholder="Enter event description"
+              onChange={(e) => setDescription(e.target.value)}
+            />
           </FormControl>
 
           <FormControl>
-            <FormLabel htmlFor="accessibility" fontWeight="bold">Accessibility Accommodation</FormLabel>
-            <Input id="accessibility" placeholder="Enter accessibility accommodation" onChange={(e) => setAccessibilityAccommodation(e.target.value)} />
+            <FormLabel htmlFor="accessibility" fontWeight="bold">
+              Accessibility Accommodation
+            </FormLabel>
+            <Input
+              id="accessibility"
+              placeholder="Enter accessibility accommodation"
+              onChange={(e) => setAccessibilityAccommodation(e.target.value)}
+            />
           </FormControl>
 
           <FormControl>
-            <FormLabel htmlFor="required-items" fontWeight="bold">Required Items</FormLabel>
-            <Input id="required-items" placeholder="Enter required items" onChange={(e) => setRequiredItems(e.target.value)} />
+            <FormLabel htmlFor="required-items" fontWeight="bold">
+              Required Items
+            </FormLabel>
+            <Input
+              id="required-items"
+              placeholder="Enter required items"
+              onChange={(e) => setRequiredItems(e.target.value)}
+            />
           </FormControl>
         </VStack>
         <Box flex="1">
-          <Text fontWeight="bold" ml={4}>Date/Time</Text>
+          <Text fontWeight="bold" ml={4}>
+            Date/Time
+          </Text>
           {/* MiniCalendar */}
           <MiniCalendar />
         </Box>
       </Flex>
-      <Button colorScheme="blue" alignSelf="center" onClick={handleCreateEvent}>
-            Create Event
-      </Button>
+      <Box display="flex" justifyContent="center" mt={4}>
+        <Button
+          colorScheme="blue"
+          onClick={handleCreateEvent}
+          minWidth="150px"
+          width="20%"
+        >
+          Create Event
+        </Button>
+      </Box>
     </Box>
   );
 };
