2dbea500077a43625b0c220684027cc6435aa882
Jesus Avalos <jesusavalosr2004@gmail.com>
2024-03-20 13:22:20 -0700

    update: digital waiver schema

--- src/database/digitalWaiverSchema.ts
+++ src/database/digitalWaiverSchema.ts
@@ -1,10 +1,16 @@
 import mongoose, { Schema } from "mongoose";
 
 export type IWaiver = {
+    _id: string;
+    eventId: Schema.Types.ObjectId;
     dependents: string[];
 };
 
 const digitalWaiverSchema = new Schema({
+    eventId: {
+        type: Schema.Types.ObjectId,
+        required: true,
+    },
     dependents: {
         type: [String],
         default: [],
