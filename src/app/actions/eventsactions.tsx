7079b8e4ac9597e6bdc8f02c4b2a2f95c773f045
Jesus Avalos <jesusavalosr2004@gmail.com>
2024-04-07 12:08:13 -0700

    updated event actions

--- src/app/actions/eventsactions.tsx
+++ src/app/actions/eventsactions.tsx
@@ -3,12 +3,12 @@
 import connectDB from "@database/db";
 import Event from "@database/eventSchema"
 
-export const getEvents = async() => {
+export const getEvents = async (offset: number, limit: number) => {
     await connectDB(); // connect to db
 
     try {
         // query for all events and sort by date
-        const events = await Event.find().sort({ date: -1 }).orFail();
+        const events = await Event.find().sort({ date: -1 }).skip(offset).limit(limit).orFail();
         return events;
     } catch (err) {
         return [];
