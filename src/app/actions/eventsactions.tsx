249721f107d4fcd5901c6d1a36356fdc4eae13d8
Jesus Avalos <jesusavalosr2004@gmail.com>
2024-04-09 22:38:57 -0700

    server-actions: event action offset / limit added

--- src/app/actions/eventsactions.tsx
+++ src/app/actions/eventsactions.tsx
@@ -6,9 +6,18 @@ import Event from "@database/eventSchema"
 export const getEvents = async (offset: number, limit: number) => {
     await connectDB(); // connect to db
 
+    const currentDate = new Date();
     try {
+        let events = null;
         // query for all events and sort by date
-        const events = await Event.find().skip(offset).limit(limit).sort({ date: -1 }).orFail();
+        if (offset < 0){
+            // query upcoming events
+            events = await Event.find({startTime: {$gte : currentDate}}).sort({startTime: 1})
+        }
+        else{
+            events = await Event.find({startTime: {$lt: currentDate}}).skip(offset).limit(limit).sort({ date: -1 }).orFail();
+            console.log(events)
+        }
         return JSON.stringify(events);
     } catch (err) {
         return null;
