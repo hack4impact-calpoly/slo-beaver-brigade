c448384c07d1707af5d0e998818a3667338771ca
Shuya Yanase <your-email@email.com>
2024-04-14 15:13:06 -0700

    profile page

--- src/app/components/EditProfile.tsx
+++ src/app/components/EditProfile.tsx
@@ -1,172 +1,178 @@
 "use client";
 
+
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
 import styles from "../styles/profile/profile.module.css";
 import { PencilIcon } from "@heroicons/react/16/solid";
-import { useUser } from "@clerk/nextjs"; // Import useUser from Clerk
-import { currentUser } from "@clerk/nextjs";
+import { useUser } from "@clerk/clerk-react";
+
 
 interface UserData {
-  firstName: string;
-  lastName: string;
-  userName: string;
-  email: string;
-  zipCode: string;
-  preferredLanguage: string;
-  phoneNumber: string;
-  password: string;
-  city: string;
-  zipcode: string;
-  state: string;
-  receiveEmails: string;
+ firstName: string;
+ lastName: string;
+ username: string;
+ email: string;
+ zipcode: string;
+ preferredLanguage: string;
+ phoneNumber: string;
+ password: string;
+ city: string;
+ state: string;
+ receiveEmails: string;
 }
 
-function EditProfile() {
-  //const { userId } = useParams<{ userId: string }>();
-  const [userData, setUserData] = useState("");
 
-  const { isSignedIn, user, isLoaded } = useUser();
+const ProfileEdit: React.FC = () => {
+ const [userData, setUserData] = useState<UserData>({
+   firstName: "",
+   lastName: "",
+   username: "",
+   email: "",
+   zipcode: "",
+   preferredLanguage: "",
+   phoneNumber: "",
+   password: "",
+   city: "",
+   state: "",
+   receiveEmails: "",
+ });
 
-  console.log('wow')
-  console.log(user, isSignedIn)
-  if (isSignedIn) {
-    const userId = user.unsafeMetadata["dbId"]; // Assuming this is the correct ID to match against event attendees
 
-    console.log("hi")
-    console.log("wow: " + userId)
-    // Fetch all events
-    const eventsResponse = fetch("/api/events");
-    
+ const { isSignedIn, user, isLoaded } = useUser();
 
-  
 
-    
-  }
+ useEffect(() => {
+   if (isSignedIn && isLoaded) {
+     setUserData({
+       firstName: user.firstName || "",
+       lastName: user.lastName || "",
+       username: user.username || "",
+       email: user.primaryEmailAddress?.emailAddress  || "",
+       zipcode: String(user.unsafeMetadata["phone"])  || "",
+       preferredLanguage: "",
+       phoneNumber: String(user.unsafeMetadata["phone"])  || "",
+       password: "",
+       city: "",
+       state: "",
+       receiveEmails: "",
+     });
+   }
+ }, [isSignedIn, isLoaded]);
 
-  // useEffect(() => {
-  //   // Fetch additional user data using Clerk's user ID (dbId)
-  //   axios
-  //     .get(`/api/user/${userId}`)
-  //     .then((response) => {
-  //       setUserData(response.data);
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error fetching user data:", error);
-  //     });
-  // }, [userData]);
 
-  
 
-  // console.log("Bro" + userData);
-  return (
-    <div className={styles.profileContainer}>
-      <div className={styles.formContainer}>
-        <div className={`${styles.formGroup} ${styles.accountDetails}`}>
-          <div className={`${styles.highlight} ${styles.accountHeader}`}>
-            <h2 className={`${styles.title} ${styles.biggerTitle}`}>
-              Account Details
-            </h2>
-          </div>
-          <div className={styles.formFields}>
-            <div>
-              <p>
-                <strong>Username</strong> <br />
-                {user?.username}
-                <button className={styles.editFieldButton}>
-                  Edit Username
-                </button>
-              </p>
-              <p>
-                <strong>Current Password</strong> <br />
-                {userData.password}
-                <button className={styles.editFieldButton}>
-                  Edit Password
-                </button>
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className={`${styles.formGroup} ${styles.personalDetails}`}>
-          <div className={`${styles.highlight} ${styles.personalHeader}`}>
-            <h2 className={`${styles.title} ${styles.biggerTitle}`}>
-              Personal Details
-            </h2>
-            <PencilIcon className={styles.pencilIcon} />
-            <h2 className={styles.editButton}>Edit Details</h2>
-          </div>
-          <div className={styles.formFields}>
-            <div>
-              <p>
-                <strong>First Name</strong>
-                <br /> {user?.firstName}
-              </p>
-              <p>
-                <strong>Last Name</strong>
-                <br /> {user?.lastName}
-              </p>
-              <p>
-                <strong>Preferred Language</strong>
-                <br /> {userData.preferredLanguage}
-              </p>
-            </div>
-            <div>
-              <p>
-                <strong>Telephone Number</strong>
-                <br /> {user?.primaryPhoneNumber?.phoneNumber}
-              </p>
-              <p>
-                <strong>Email</strong>
-                <br /> {user?.primaryEmailAddress?.emailAddress}
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className={`${styles.formGroup} ${styles.savedAddress}`}>
-          <div className={`${styles.highlight} ${styles.savedAddressHeader}`}>
-            <h2 className={`${styles.title} ${styles.biggerTitle}`}>
-              Saved Address
-            </h2>
-            <PencilIcon className={styles.pencilIcon} />
-            <h2 className={styles.editButton}>Edit Details</h2>
-          </div>
-          <div className={styles.formFields}>
-            <div>
-              <p>
-                <strong>City</strong>
-                <br /> {userData.city}
-              </p>
-              <p>
-                <strong>Zipcode</strong>
-                <br /> {userData.zipcode}
-              </p>
-            </div>
-            <div>
-              <p>
-                <strong>State</strong>
-                <br /> {userData.state}
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className={`${styles.formGroup} ${styles.savedAddress}`}>
-          <div className={`${styles.highlight} ${styles.personalHeader}`}>
-            <h2 className={`${styles.title} ${styles.biggerTitle}`}></h2>
-            <PencilIcon className={styles.pencilIcon} />
-            <h2 className={styles.editButton}>Edit Details</h2>
-          </div>
-          <div className={styles.formFields}>
-            <div>
-              <p>
-                <strong>Receive Emails from SLO Beaver Brigrade</strong> <br />
-                {userData.receiveEmails}
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+
+ return (
+   <div className={styles.profileContainer}>
+     <div className={styles.formContainer}>
+       <div className={`${styles.formGroup} ${styles.accountDetails}`}>
+         <div className={`${styles.highlight} ${styles.accountHeader}`}>
+           <h2 className={`${styles.title} ${styles.biggerTitle}`}>
+             Account Details
+           </h2>
+         </div>
+         <div className={styles.formFields}>
+           <div>
+             <p>
+               <strong>Username</strong> <br />
+               {userData.username}
+               <button className={styles.editFieldButton}>
+                 Edit Username
+               </button>
+             </p>
+             <p>
+               <strong>Current Password</strong> <br />
+               {userData.password}
+               <button className={styles.editFieldButton}>
+                 Edit Password
+               </button>
+             </p>
+        
+            
+           </div>
+         </div>
+       </div>
+       <div className={`${styles.formGroup} ${styles.personalDetails}`}>
+         <div className={`${styles.highlight} ${styles.personalHeader}`}>
+           <h2 className={`${styles.title} ${styles.biggerTitle}`}>
+             Personal Details
+           </h2>
+           <PencilIcon className={styles.pencilIcon} />
+           <h2 className={styles.editButton}>Edit Details</h2>
+         </div>
+         <div className={styles.formFields}>
+           <div>
+             <p>
+               <strong>First Name</strong>
+               <br /> {userData.firstName}
+             </p>
+             <p>
+               <strong>Last Name</strong>
+               <br /> {userData.lastName}
+             </p>
+             <p>
+               <strong>Preferred Language</strong>
+               <br /> {userData.preferredLanguage}
+             </p>
+           </div>
+           <div>
+             <p>
+               <strong>Telephone Number</strong>
+               <br /> {userData.phoneNumber}
+             </p>
+             <p>
+               <strong>Email</strong>
+               <br /> {userData.email}
+             </p>
+           </div>
+         </div>
+       </div>
+       <div className={`${styles.formGroup} ${styles.savedAddress}`}>
+         <div className={`${styles.highlight} ${styles.savedAddressHeader}`}>
+           <h2 className={`${styles.title} ${styles.biggerTitle}`}>
+             Saved Address
+           </h2>
+           <PencilIcon className={styles.pencilIcon} />
+           <h2 className={styles.editButton}>Edit Details</h2>
+         </div>
+         <div className={styles.formFields}>
+           <div>
+             <p>
+               <strong>City</strong>
+               <br /> {userData.city}
+             </p>
+             <p>
+               <strong>Zipcode</strong>
+               <br /> {userData.zipcode}
+             </p>
+           </div>
+           <div>
+             <p>
+               <strong>State</strong>
+               <br /> {userData.state}
+             </p>
+           </div>
+         </div>
+       </div>
+       <div className={`${styles.formGroup} ${styles.savedAddress}`}>
+         <div className={`${styles.highlight} ${styles.personalHeader}`}>
+           <h2 className={`${styles.title} ${styles.biggerTitle}`}></h2>
+           <PencilIcon className={styles.pencilIcon} />
+           <h2 className={styles.editButton}>Edit Details</h2>
+         </div>
+         <div className={styles.formFields}>
+           <div>
+             <p>
+               <strong>Receive Emails from SLO Beaver Brigrade</strong> <br />
+               {userData.receiveEmails}
+             </p>
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
+ );
 };
 
-export default EditProfile;
+
+export default ProfileEdit;
