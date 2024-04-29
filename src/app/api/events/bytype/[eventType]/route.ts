789e962c0bcdbe482137c7dd11c275b7bb845094
HanYu Wu <hanyuwu04@gmail.com>
2024-04-28 17:37:26 -0700

    created GET api call to fetch distinct event types

---
+++ src/app/api/events/bytype/[eventType]/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+import connectDB from "@database/db";
+import Event, { IEvent } from "@database/eventSchema";
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type IParams = {
+    params: {
+        eventType: string;
+    };
+};
+
+export async function GET(req: NextRequest, { params }: IParams) {
+    await connectDB(); // connect to db
+    const { eventType } = params; // another destructure
+    try {
+        const eventTypes = await Event.distinct(eventType).orFail();
+        return NextResponse.json(eventTypes);
+    } catch (err) {
+        return NextResponse.json(
+            "Failed to fetch event types: " + err,
+            { status: 404 }
+        );
+    }
+}
