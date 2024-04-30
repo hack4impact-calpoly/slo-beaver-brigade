a99b228f0182c4108c181711d68579e53e34d771
Jesus Avalos <jesusavalosr2004@gmail.com>
2024-04-29 19:43:32 -0700

    fixed issue with only showing 10 events in admin events

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import { UserDashboard } from "@components/UserDashboard";
 import { getUserDbData } from "app/lib/authentication";
+import { getBaseUrl } from "app/lib/random";
 import { Suspense } from "react";
 
 const getAllEvents = async () => {
 
-    const res = await fetch("http://localhost:3000/api/events", {
+    const res = await fetch(getBaseUrl() + "/api/events", {
+        cache: "force-cache",
         next: {
             tags: ['events']
         }
