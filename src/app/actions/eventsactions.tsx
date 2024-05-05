58283925d604e5e4efdbd00b6407e8be45099312
Jesus Avalos <jesusavalosr2004@gmail.com>
2024-05-04 17:03:45 -0700

    events using server component

--- src/app/actions/eventsactions.tsx
+++ src/app/actions/eventsactions.tsx
@@ -26,3 +26,23 @@ export const getEvents = async (offset: number, limit: number) => {
         return null;
     }
 }
+
+export async function getSelectedEvents(selectedFilters: string[]) {
+  await connectDB(); // connect to db
+  try {
+    let query = Event.find().sort({ date: -1 });
+
+    // Apply filters if any selected
+    if (selectedFilters.length > 0) {
+      query = query.where("eventType").in(selectedFilters);
+    }
+
+    // Execute the query
+    const events = await query.exec();
+
+    // returns all events in json format or errors
+    return JSON.stringify(events);
+  } catch (err) {
+    return "[]";
+  }
+}
