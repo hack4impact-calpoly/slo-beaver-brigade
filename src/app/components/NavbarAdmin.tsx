893b3d3d114db5017e27c5434f101353ddc60484
Tyler <tylerk516@gmail.com>
2024-05-21 19:17:27 -0700

    changed Manage Events to route to /admin/events

--- src/app/components/NavbarAdmin.tsx
+++ src/app/components/NavbarAdmin.tsx
@@ -63,7 +63,7 @@ export default function Navbar(props: { name: string }) {
                   </Link>
                 </li>
                 <li>
-                  <Link href="/" onClick={handleHideNavbar}>
+                  <Link href="/admin/events" onClick={handleHideNavbar}>
                     Manage Events
                   </Link>
                 </li>
