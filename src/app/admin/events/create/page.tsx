9bb2bdc1441e346f5bba6aeb3a1b147b74a8a236
anivishy <ani.vishy@outlook.com>
2025-01-25 14:26:26 -0800

    feat add private event structure

--- src/app/admin/events/create/page.tsx
+++ src/app/admin/events/create/page.tsx
@@ -42,18 +42,20 @@ type Group = {
 };
 
 export default function Page() {
-    const {mutate} = useEventsAscending()
+  const { mutate } = useEventsAscending();
   const toast = useToast();
   const router = useRouter();
   const [eventName, setEventName] = useState("");
+  const [isPrivateEvent, setIsPrivateEvent] = useState<boolean>(false); // adding private event const
+  //const [showSecondSection, setShowSecondSection] = useState(false);
   const [coverImage, setCoverImage] = useState<File | null>(null);
   const [imagePreview, setImagePreview] = useState<string | null>(null);
   const [eventType, setEventType] = useState("");
   const [organizationIds, setOrganizationIds] = useState<string[]>([]);
-  const [groupsSelected, setGroupsSelected] = useState<IGroup[]>([])
+  const [groupsSelected, setGroupsSelected] = useState<IGroup[]>([]);
   // Specify type for group to avoid error
-  const {groups, isLoading, isError, mutateGroups} = useGroups()
- const setGroups = useCallback((updateFunction: (groups: any[]) => any[]) => {
+  const { groups, isLoading, isError, mutateGroups } = useGroups();
+  const setGroups = useCallback((updateFunction: (groups: any[]) => any[]) => {
     mutateGroups((currentGroups) => {
       if (currentGroups) {
         return updateFunction(currentGroups);
@@ -65,15 +67,15 @@ export default function Page() {
   const [location, setLocation] = useState("");
   const [language, setLanguage] = useState("Yes");
   const [description, setDescription] = useState("");
-  const [accessibilityAccommodation, setAccessibilityAccommodation] = useState("Yes");
+  const [accessibilityAccommodation, setAccessibilityAccommodation] =
+    useState("Yes");
   const [checkList, setChecklist] = useState("N/A");
   const [eventStart, setEventStart] = useState("");
   const [eventEnd, setEventEnd] = useState("");
   const [activeDate, setActiveDate] = useState("");
   const [eventTypes, setEventTypes] = useState<string[]>([]);
-  const [onlyInvitees, setOnlyInvitees] = useState<boolean>(false)
-  const [sendEmailInvitees, setSendEmailInvitees] = useState<boolean>(false)
-
+  //const [onlyInvitees, setOnlyInvitees] = useState<boolean>(false)
+  const [sendEmailInvitees, setSendEmailInvitees] = useState<boolean>(false);
 
   const handleEventNameChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     setEventName(e.target.value);
@@ -96,9 +98,9 @@ export default function Page() {
   //Parse and format start and end time from user input
   const handleTimeChange = (start: string, end: string) => {
     // Format for parsing input times (handle both 12-hour and 24-hour formats)
-    if(start && end){
+    if (start && end) {
       const timeFormat =
-      start.includes("AM") || start.includes("PM") ? "h:mm a" : "HH:mm";
+        start.includes("AM") || start.includes("PM") ? "h:mm a" : "HH:mm";
 
       // Parse the start and end times as dates on the active date
       const parsedStartTime = parse(
@@ -111,20 +113,20 @@ export default function Page() {
         timeFormat,
         new Date(`${activeDate}T00:00:00`)
       );
-        
+
       // Format the adjusted dates back into ISO strings
       const formattedStartDateTime = formatISO(parsedStartTime);
       const formattedEndDateTime = formatISO(parsedEndTime);
       // Update the state with the formatted date times
       setEventStart(formattedStartDateTime);
       setEventEnd(formattedEndDateTime);
-    };
-    if(!start){
+    }
+    if (!start) {
       setEventStart("");
-    };
-    if(!end){
+    }
+    if (!end) {
       setEventEnd("");
-    };
+    }
   };
   // Update active date upon change from MiniCalendar
   const handleDateChangeFromCalendar = (newDate: string) => {
@@ -143,7 +145,7 @@ export default function Page() {
 
   // Handle file selection for the event cover image and set preview
   const handleImageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPreselected(false)
+    setPreselected(false);
     const file = e.target.files ? e.target.files[0] : null;
     if (file) {
       const reader = new FileReader();
@@ -156,7 +158,6 @@ export default function Page() {
       setCoverImage(file);
     }
   };
-
 
   // Throw a Toast when event details are not complete and makes a post request to create event if details are complete
   const handleCreateEvent = async () => {
@@ -177,10 +178,7 @@ export default function Page() {
         isClosable: true,
       });
       return;
-    }
-    else if (
-      eventStart === "" || eventEnd === "" || activeDate === ""
-    ){
+    } else if (eventStart === "" || eventEnd === "" || activeDate === "") {
       toast({
         title: "Error",
         description: "Event date and time are not set",
@@ -189,10 +187,7 @@ export default function Page() {
         isClosable: true,
       });
       return;
-    }
-    else if(
-      eventEnd < eventStart
-    ){
+    } else if (eventEnd < eventStart) {
       toast({
         title: "Error",
         description: "End time is before start time",
@@ -206,26 +201,25 @@ export default function Page() {
     // Try to upload image
     const file = fileInputRef?.current?.files?.[0] ?? null;
     let imageurl = null;
-    
+
     if (file || preselected) {
-        if (!preselected){
-    imageurl = await uploadFileS3Bucket(file);
+      if (!preselected) {
+        imageurl = await uploadFileS3Bucket(file);
         if (!imageurl) {
-            console.error("Failed to create the event: image upload.");
-            toast({
+          console.error("Failed to create the event: image upload.");
+          toast({
             title: "Error",
             description: "Failed to create the event",
             status: "error",
             duration: 2500,
             isClosable: true,
-            });
-            return;
-        }    }
-        else{
-            imageurl = imagePreview
+          });
+          return;
         }
-        }
-   
+      } else {
+        imageurl = imagePreview;
+      }
+    }
 
     const eventData = {
       eventName,
@@ -239,13 +233,12 @@ export default function Page() {
       startTime: eventStart,
       endTime: eventEnd,
       volunteerEvent: eventType === "Volunteer",
-      groupsAllowed: groupsSelected.map(group => group._id as string),
-      groupsOnly: onlyInvitees
+      groupsAllowed: groupsSelected.map((group) => group._id as string),
+      //groupsOnly: onlyInvitees
     };
 
     // Attempt to create event via API and handle response
     try {
-        
       const response = await fetch("/api/events", {
         method: "POST",
         headers: {
@@ -258,24 +251,30 @@ export default function Page() {
         throw new Error("HTTP error! status: $(response.status)");
       }
 
-      const event: IEvent= await response.json();
+      const event: IEvent = await response.json();
       // send confirmation email if button was checked
-      if (sendEmailInvitees){
-        const res = await fetch('/api/events/' + event._id + "/groups/confirmation", 
-            {method: 'POST',
-            body: JSON.stringify({groupIds: groupsSelected.flatMap(group => group._id)})})
-        if (!res){
-            toast()
-        toast({
-                title: "Error",
-                description: "Failed to send emails.",
-                status: "error",
-                duration: 2500,
-                isClosable: true,
-            });
-                }
+      if (sendEmailInvitees) {
+        const res = await fetch(
+          "/api/events/" + event._id + "/groups/confirmation",
+          {
+            method: "POST",
+            body: JSON.stringify({
+              groupIds: groupsSelected.flatMap((group) => group._id),
+            }),
+          }
+        );
+        if (!res) {
+          toast();
+          toast({
+            title: "Error",
+            description: "Failed to send emails.",
+            status: "error",
+            duration: 2500,
+            isClosable: true,
+          });
+        }
       }
-        mutate()
+      mutate();
       toast({
         title: "Event Created",
         description: "Your event has been successfully created.",
@@ -352,16 +351,15 @@ export default function Page() {
 
   // Fetch groups data on component mount
   useEffect(() => {
-    if (isError){
-        toast({
-          title: "Error",
-          description: "Failed to fetch groups",
-          status: "error",
-          duration: 2500,
-          isClosable: true,
-        });
+    if (isError) {
+      toast({
+        title: "Error",
+        description: "Failed to fetch groups",
+        status: "error",
+        duration: 2500,
+        isClosable: true,
+      });
     }
-
   }, [isError]);
 
   // Fetching different event types
@@ -382,26 +380,28 @@ export default function Page() {
     fetchEventTypes();
   }, []);
 
-  
-  
   return (
     <Box p={8} mx="10">
       <Text fontSize="2xl" fontWeight="bold" color="black" mt={-12} mb={3}>
         Create New Event
       </Text>
 
-    {/* image uploading */}
-    <Flex flexDir={{ base: "column", md: "row" }} flex="1" gap={{base: "10px", md: "50px"}}>
+      {/* image uploading */}
+      <Flex
+        flexDir={{ base: "column", md: "row" }}
+        flex="1"
+        gap={{ base: "10px", md: "50px" }}
+      >
         <FormControl mb="4" onClick={promptFileInput} cursor="pointer">
-            <Input
+          <Input
             id="cover-image"
             type="file"
             accept="image/*"
             onChange={handleImageChange}
             ref={fileInputRef}
             hidden // Hide the actual input
-            />
-            <Box
+          />
+          <Box
             position="relative"
             borderWidth="1px"
             p="4"
@@ -416,14 +416,18 @@ export default function Page() {
             justifyContent="center"
             alignItems="center"
             flexDirection="column"
-            >
+          >
             {!imagePreview ? (
-                <>
+              <>
                 <Text>Upload Image</Text>
-                <IconButton aria-label="Upload image" icon={<AddIcon />} mt="2" />
-                </>
+                <IconButton
+                  aria-label="Upload image"
+                  icon={<AddIcon />}
+                  mt="2"
+                />
+              </>
             ) : (
-                <Image
+              <Image
                 src={imagePreview}
                 alt="Event cover preview"
                 position="absolute"
@@ -433,11 +437,14 @@ export default function Page() {
                 height="100%"
                 objectFit="cover"
                 zIndex={0}
-                />
+              />
             )}
-            </Box>
+          </Box>
         </FormControl>
-        <ImageSelector setPreselected={setPreselected} setImageURL={setImagePreview}></ImageSelector>
+        <ImageSelector
+          setPreselected={setPreselected}
+          setImageURL={setImagePreview}
+        ></ImageSelector>
       </Flex>
 
       <Flex direction={{ base: "column", md: "row" }} gap={20} mb={6}>
@@ -466,7 +473,6 @@ export default function Page() {
                   label: type,
                 }))}
                 onChange={(option) => {
-                  
                   setEventType(option ? option.value : "");
                 }}
                 chakraStyles={{
@@ -491,9 +497,9 @@ export default function Page() {
                   value: group,
                   label: group.group_name,
                 }))}
-                value={groupsSelected.map(group => ({
-                    value: group,
-                    label: group.group_name,
+                value={groupsSelected.map((group) => ({
+                  value: group,
+                  label: group.group_name,
                 }))}
                 onChange={(selectedOptions) =>
                   setGroupsSelected(
@@ -515,6 +521,81 @@ export default function Page() {
               />
             </FormControl>
           </HStack>
+
+          <Flex direction={{ base: "column", md: "row" }} gap={20} mb={6}>
+            <VStack spacing={4} align="stretch" flex="1">
+              {/* Private Event Section */}
+              <HStack justifyContent="space-between">
+                <FormControl width="48%">
+                  <FormLabel htmlFor="private-event" fontWeight="bold">
+                    Private Events
+                  </FormLabel>
+                  <div className="flex sm:flex-row flex-col-reverse gap-5 sm:gap-10 sm:items-center">
+                    <div className="flex flex-row gap-4 justify-center">
+                      Is this a private event?
+                      <Checkbox
+                        checked={isPrivateEvent}
+                        onChange={() => setIsPrivateEvent((prev) => !prev)}
+                      />
+                    </div>
+                  </div>
+                </FormControl>
+
+                {isPrivateEvent && groups && (
+                  <div className="flex sm:flex-row flex-col-reverse gap-5 sm:gap-10 sm:items-center">
+                    <CreateTemporaryGroup
+                      groups={groupsSelected}
+                      mutate={mutateGroups}
+                      setGroups={setGroupsSelected}
+                    />
+                    <div className="flex flex-row gap-4 justify-center">
+                      Notify Invitees:
+                      <Checkbox
+                        checked={sendEmailInvitees}
+                        onChange={() =>
+                          setSendEmailInvitees((checked) => !checked)
+                        }
+                      />
+                    </div>
+                  </div>
+                )}
+              </HStack>
+            </VStack>
+          </Flex>
+
+          {/* <div className="text-xl font-bold mb-4">Private Events</div>
+
+          <HStack justifyContent="space-between">
+            {groups && (
+              <div className="flex sm:flex-row flex-col-reverse gap-5 sm:gap-10 sm:items-center">
+                <div className="flex flex-row gap-4 justify-center">
+                  Is this a private event?:
+                  <Checkbox
+                    checked={isPrivateEvent}
+                    onChange={() => setIsPrivateEvent((prev) => !prev)}
+                  />
+                </div>
+              </div>
+            )}
+              
+
+            {isPrivateEvent && groups && (
+              <div className="flex sm:flex-row flex-col-reverse gap-5 sm:gap-10 sm:items-center">
+                <CreateTemporaryGroup
+                  groups={groupsSelected}
+                  mutate={mutateGroups}
+                  setGroups={setGroupsSelected}
+                />
+                <div className="flex flex-row gap-4 justify-center">
+                  Notify Invitees:
+                  <Checkbox
+                    checked={sendEmailInvitees}
+                    onChange={() => setSendEmailInvitees((checked) => !checked)}
+                  />
+                </div>
+              </div>
+            )}
+          </HStack> */}
 
           <FormControl isRequired>
             <FormLabel htmlFor="location" fontWeight="bold">
@@ -573,7 +654,7 @@ export default function Page() {
             />
           </FormControl>
 
-            <FormControl>
+          {/* <FormControl>
             <FormLabel htmlFor="invitees" fontWeight="bold">
               Invitees Only
             </FormLabel>
@@ -595,24 +676,34 @@ export default function Page() {
                 }),
               }}
             />
-          </FormControl>
-          {onlyInvitees && groups && <div className="flex sm:flex-row flex-col-reverse gap-5 sm:gap-10 sm:items-center ">
+          </FormControl> */}
+          {/* {onlyInvitees && groups && <div className="flex sm:flex-row flex-col-reverse gap-5 sm:gap-10 sm:items-center ">
           <CreateTemporaryGroup groups={groupsSelected} mutate={mutateGroups} setGroups={setGroupsSelected}/>
           <div className="flex flex-row gap-4 justify-center"> Notify Invitees: <Checkbox checked={sendEmailInvitees} onChange={() => setSendEmailInvitees((checked) => !checked)}></Checkbox></div>
-            </div>}
+            </div>} */}
 
           <FormControl isRequired>
             <FormLabel htmlFor="description" fontWeight="bold">
               Description
             </FormLabel>
-            <MDEditor className={style.preview} value={description} onChange={(e) => setDescription(e || "")} data-color-mode="light"/>
+            <MDEditor
+              className={style.preview}
+              value={description}
+              onChange={(e) => setDescription(e || "")}
+              data-color-mode="light"
+            />
           </FormControl>
 
           <FormControl>
             <FormLabel htmlFor="required-items" fontWeight="bold">
               Checklist
             </FormLabel>
-            <MDEditor className={style.preview} value={checkList} onChange={(e) => setChecklist(e || "")} data-color-mode="light"/>
+            <MDEditor
+              className={style.preview}
+              value={checkList}
+              onChange={(e) => setChecklist(e || "")}
+              data-color-mode="light"
+            />
           </FormControl>
         </VStack>
         <Flex flex="1">
@@ -632,9 +723,9 @@ export default function Page() {
       </Flex>
       <Box display="flex" justifyContent="center" mt={4}>
         <Button
-          loadingText="Creating"                 
-          bg="#e0af48"   
-          color="black"
+          loadingText="Creating"
+          bg="#e0af48"
+          color="white"
           _hover={{ bg: "#C19137" }}
           onClick={handleCreateEvent}
           minWidth="150px"
