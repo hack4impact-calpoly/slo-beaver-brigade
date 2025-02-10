bbc695852a22649824bd83998a536c1cf99fcd90
emlai1263 <nicecherrim@gmail.com>
2025-02-10 15:34:46 -0800

    feat: admin page base

---
+++ src/app/components/AuditPreview.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React from "react";
+import style from "@styles/admin/audit.module.css";
+import { Audit } from "app/lib/audit";
+import { Card, CardBody, Heading, Text } from "@chakra-ui/react";
+import { CgProfile } from "react-icons/cg";
+import { RiArrowRightSLine } from "react-icons/ri";
+
+const AuditPreview = () => {
+  return (
+    <Card
+      className={style.auditPreview}
+      role="button"
+      onClick={() =>
+        console.log("Audit preview clicked (expansion to be implemented)")
+      }
+    >
+      <CardBody className={style.auditContent}>
+        <CgProfile className={style.auditIcon} />
+        <div className={style.auditText}>
+          <Heading size="md">
+            [Admin] added created a new event: Watery Walk
+          </Heading>
+          <Text>Today at 2:24pm</Text>
+        </div>
+        <RiArrowRightSLine className={style.auditIcon} />
+      </CardBody>
+    </Card>
+  );
+};
+
+export default AuditPreview;
