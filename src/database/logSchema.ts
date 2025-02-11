6bb6f8c2044b7f4764a05390ace527cd75af6fc5
vmagdangal <vmagdangal257@gmail.com>
2025-02-10 17:54:11 -0800

    feat: create logSchema file

---
+++ src/database/logSchema.ts
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from "mongoose";
+
+export type ILog = {
+    _id: string;
+    user: Schema.Types.ObjectId;
+    action: string;
+    date: Date;
+    link: Schema.Types.ObjectId;
+};
+
+const logSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        required: true,
+    },
+    action: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    link: {
+        type: Schema.Types.ObjectId,
+        required: true,
+    },
+});
+
+const Log =
+    mongoose.models["logTest"] || mongoose.model("logTest", logSchema);
+
+export default Log;
