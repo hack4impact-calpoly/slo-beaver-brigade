d6d5baf0fbcb04bbdfa8bb75dd5460241e3446bd
Jason Jelincic <114632851+SilveerDusk@users.noreply.github.com>
2025-02-21 23:32:42 -0800

    Update page.tsx

--- src/app/admin/audit/page.tsx
+++ src/app/admin/audit/page.tsx
@@ -7,7 +7,6 @@ import { Text, Input, Select, Spinner, Center } from "@chakra-ui/react";
 import useSWR from "swr";
 import { ILog } from "@/database/logSchema";
 
-
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 const AuditPage = () => {
   const { data: logs, error, isLoading } = useSWR<ILog[]>('/api/logs', fetcher);
