376f70e56053a35bd8b0df50490da4238fe80668
Adarsh132500 <adarsmuru@gmail.com>
2025-02-24 19:11:21 -0800

    Changed event template schema

--- src/database/eventTemplateSchema.ts
+++ src/database/eventTemplateSchema.ts
@@ -1,64 +1,63 @@
 import mongoose, { Schema } from "mongoose";
 
-export interface IEventTemplate {
-  _id: string;
-  eventName: string;
-  location: string;
-  description: string;
-  wheelchairAccessible: boolean;
-  spanishSpeakingAccommodation: boolean;
-  volunteerEvent: boolean;
-  groupsAllowed: string[] | null;
-  registeredIds: string[];
-  startTime: Date;
-  endTime: Date;
-}
+export type IEvent = {
+    _id: string;
+    eventName: string;
+    eventImage: string | null;
+    eventType: string | null;
+    groupsOnly?: boolean;
+    location: string;
+    description: string;
+    checklist: string;
+    wheelchairAccessible: boolean;
+    spanishSpeakingAccommodation: boolean;
+    startTime: Date;
+    endTime: Date;
+    volunteerEvent: boolean;
+    groupsAllowed: string[];
+    attendeeIds: string[];
+    registeredIds: string[];
+};
 
+// Mongoose schema
+// automatically converts object ids to strings
+mongoose.Schema.ObjectId.get((v) => v.toString());
+mongoose.Schema.Types.Date.get((v) => v.toString());
 const eventTemplateSchema = new Schema({
-  eventName: {
-    type: String,
-    required: true,
-  },
-  location: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  wheelchairAccessible: {
-    type: Boolean,
-    default: false,
-  },
-  spanishSpeakingAccommodation: {
-    type: Boolean,
-    default: false,
-  },
-  volunteerEvent: {
-    type: Boolean,
-    default: false,
-  },
-  groupsAllowed: {
-    type: [String],
-    default: null,
-  },
-  registeredIds: {
-    type: [String],
-    default: [],
-  },
-  startTime: {
-    type: Date,
-    required: true,
-  },
-  endTime: {
-    type: Date,
-    required: true,
-  },
+    eventName: { type: String, required: true },
+    eventImage: { type: String, required: false },
+    eventType: { type: String, required: false },
+    location: { type: String, required: true },
+    description: { type: String, required: true },
+    checklist: { type: String, required: false, default: "N/A" },
+    groupsOnly: { type: Boolean, required: false, default: false },
+    wheelchairAccessible: { type: Boolean, required: false, default: false },
+    spanishSpeakingAccommodation: {
+        type: Boolean,
+        required: false,
+        default: false,
+    },
+    startTime: { type: Date, required: true },
+    endTime: { type: Date, required: true },
+    volunteerEvent: { type: Boolean, required: true },
+    groupsAllowed: {
+        type: [Schema.Types.ObjectId],
+        required: false,
+        default: [],
+    },
+    attendeeIds: {
+        type: [Schema.Types.ObjectId],
+        required: false,
+        default: [],
+    },
+    registeredIds: {
+        type: [Schema.Types.ObjectId],
+        required: true,
+        default: [],
+    },
 });
 
-const EventTemplate =
-  mongoose.models.eventTemplatesTest ||
-  mongoose.model("eventTemplatesTest", eventTemplateSchema);
+const Event =
+    mongoose.models["eventsTemplateTest"] ?? mongoose.model("eventsTemplateTest", eventTemplateSchema);
 
-export default EventTemplate;
+export default Event;
