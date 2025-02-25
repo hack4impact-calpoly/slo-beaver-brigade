620423e1cd755dca22888a5584c3cc429e8b879c
Brady Welsh <blwelsh@calpoly.edu>
2025-02-25 11:32:05 -0800

    feat: working group DELETE route

--- src/app/api/groups/[groupId]/route.ts
+++ src/app/api/groups/[groupId]/route.ts
@@ -1,5 +1,7 @@
 import connectDB from "@database/db";
 import Group from "@database/groupSchema";
+import Event from "@database/eventSchema";
+import Log from "@database/logSchema";
 
 // Dynamic GET request to get group by ID
 export async function GET(
@@ -28,3 +30,45 @@ export async function GET(
         );
     }
 }
+
+
+export async function DELETE(request: Request, {params}: {params: { groupId: String}}){
+
+    try {
+        await connectDB();
+
+        // Destructure params to get group id
+
+        const id = params.groupId;
+
+        // Delete group from groups table
+        const group = await Group.findByIdAndDelete(id);
+
+        // Check if this deletion worked (returned a group)
+        if (!group) {
+            return Response.json({ error: "Group not found" }, { status: 404 });
+        }
+
+        // Remove all references in Events groupsAllowed array
+        await Event.updateMany(
+            {groupsAllowed: id}, 
+            {$pull: {groupsAllowed: id}},
+        );
+
+        // TODO: Will probably need to write a method for deleting group from EventTemplate
+        
+
+        return Response.json(group);
+
+
+    } catch (err: any) {
+        return Response.json(
+            { error: (err as Error).message},
+            { status: 500}
+        )
+    }
+
+
+
+
+};
