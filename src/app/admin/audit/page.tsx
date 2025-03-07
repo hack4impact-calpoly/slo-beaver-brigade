88dc7ef0e513038db5679d6d0b7a7f3aa9c1e614
Jason <Jason1@jelincic.com>
2025-03-07 14:46:02 -0800

    reverts to swr

--- src/app/admin/audit/page.tsx
+++ src/app/admin/audit/page.tsx
@@ -5,11 +5,10 @@ import style from '@styles/admin/audit.module.css';
 import MessageLog from '../../components/MessageLog';
 import { Text, Input, Select, Spinner, Center, Button } from '@chakra-ui/react';
 import { ILog } from '@/database/logSchema';
+import { useLogs } from 'app/lib/swrfunctions';
 
 const AuditPage = () => {
-  const [logs, setLogs] = useState<ILog[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
+  const {logs, isLoading, isError, mutateLogs} = useLogs();
   const [userFilter, setUserFilter] = useState('');
   const [actionFilter, setActionFilter] = useState('');
   const [mounted, setMounted] = useState(false); // Helps prevent flickering on initial load
@@ -18,19 +17,19 @@ const AuditPage = () => {
     const res = await fetch('/api/logs2', { cache: 'no-store' });
     if (!res.ok) {
       console.error('Failed to fetch logs');
-      setIsError(true);
-      setIsLoading(false);
       return;
     }
     const fetchedlogs = await res.json();
-    setLogs(fetchedlogs);
-    setIsLoading(false);
+  };
+
+  const refreshLogs = () => {
+    mutateLogs();
   };
 
   // Set mounted to true after the component mounts to prevent flickering
   useEffect(() => {
     setMounted(true); // Only on client side load
-    fetchLogs();
+    mutateLogs();
   }, []);
 
   if (!mounted) {
@@ -80,7 +79,7 @@ const AuditPage = () => {
               width="200px"
               ml={4}
             />
-            <Button onClick={fetchLogs} ml={4}>
+            <Button onClick={refreshLogs} ml={4}>
               Refresh Logs
             </Button>
           </div>
