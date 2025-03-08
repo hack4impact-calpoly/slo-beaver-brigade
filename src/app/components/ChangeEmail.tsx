3d2bc61e015c1719bd7c1f5582acd4ad41058051
Brady Welsh <blwelsh@calpoly.edu>
2025-03-08 11:47:17 -0800

    feat: intial setup for change email button

---
+++ src/app/components/ChangeEmail.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import {
+    Box,
+    Text,
+    Flex,
+    Button,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalBody,
+    ModalCloseButton,
+    Table,
+    Thead,
+    Tr,
+    Th,
+    Tbody,
+    Td,
+    useDisclosure,
+    Icon,
+    Center,
+  } from "@chakra-ui/react";
+
+function ChangeEmail() {
+
+    return <>
+        <Button>
+
+        </Button>
+    
+    
+    </>
+
+
+
+
+}
+
+export default ChangeEmail;
