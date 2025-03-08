911712990783da899dcb9207eecee8c5c3001dc0
Jason <Jason1@jelincic.com>
2025-03-07 22:28:22 -0800

    feat: adding a post route makes the logs route file dynamic

--- src/app/api/logs/route.ts
+++ src/app/api/logs/route.ts
@@ -1,25 +1,36 @@
 import connectDB from "@/database/db";
 import Log from "@/database/logSchema";
 import { NextRequest, NextResponse } from "next/server";
-export const runtime = "nodejs";
 
 // GET endpoint to fetch logs
 export async function GET(req: NextRequest) {
-    console.log(
-        "Fetching logs on server, called by:",
-        req.headers.get("referer")
-    );
     try {
         await connectDB();
 
         const logs = await Log.find().sort({ date: -1 }).limit(50); // Limit to last 50 logs for performance
 
-        console.log("Fetched logs:", logs);
         return NextResponse.json(logs);
     } catch (error) {
         console.error("Error fetching logs:", error);
         return NextResponse.json(
             { error: "Failed to fetch logs" },
+            { status: 500 }
+        );
+    }
+}
+
+// POST endpoint to create a new log
+export async function POST(req: NextRequest) {
+    try {
+        await connectDB();
+
+        const newLog = await Log.create(req.body);
+
+        return NextResponse.json(newLog, { status: 201 });
+    } catch (error) {
+        console.error("Error creating log:", error);
+        return NextResponse.json(
+            { error: "Failed to create log" },
             { status: 500 }
         );
     }
