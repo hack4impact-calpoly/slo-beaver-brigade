9a4e4b40fc0c7234170d5a581962f24c35b4330f
Jason <Jason1@jelincic.com>
2025-03-12 21:08:40 -0700

    fix: u can see the div layers now

--- src/app/components/UserProfile.tsx
+++ src/app/components/UserProfile.tsx
@@ -1,21 +1,21 @@
-import React, { useState, useEffect } from "react";
-import styles from "../styles/profile/profile.module.css";
-import { PencilIcon } from "@heroicons/react/16/solid";
-import { useUser } from "@clerk/clerk-react";
-import Link from "next/link"
-import EditProfile from "@components/EditProfile"
-import { getUserDbData } from "app/lib/authentication";
-import { IUser } from "database/userSchema";
-import "../fonts/fonts.css";
-import DeleteConfirmation from "./DeleteConfirmation";
-import ChangeEmail from "@components/ChangeEmail";
-import { StatUpArrow } from "@chakra-ui/react";
+import React, { useState, useEffect } from 'react';
+import styles from '../styles/profile/profile.module.css';
+import { PencilIcon } from '@heroicons/react/16/solid';
+import { useUser } from '@clerk/clerk-react';
+import Link from 'next/link';
+import EditProfile from '@components/EditProfile';
+import { getUserDbData } from 'app/lib/authentication';
+import { IUser } from 'database/userSchema';
+import '../fonts/fonts.css';
+import DeleteConfirmation from './DeleteConfirmation';
+import ChangeEmail from '@components/ChangeEmail';
+import { StatUpArrow } from '@chakra-ui/react';
 
 export default function UserProfile() {
   const [userData, setUserData] = useState<IUser | null>(null);
   const [loading, setLoading] = useState(true);
   const [isDataUpdated, setIsDataUpdated] = useState(false);
-  
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -23,85 +23,92 @@ export default function UserProfile() {
         if (userRes) {
           const userData = JSON.parse(userRes);
           setUserData(userData);
-          
         }
       } catch (error) {
-        console.error("Error fetching user data:", error);
+        console.error('Error fetching user data:', error);
       } finally {
         setLoading(false);
       }
     };
 
     fetchData();
-
   }, [isDataUpdated]);
 
   const closeFromChild = () => {
     return null;
-  }
+  };
 
   const displayNewInfo = () => {
     setIsDataUpdated(true);
-  }
+  };
 
- return(
-   <div className={styles.profileContainer}>
+  return (
+    <div className={styles.profileContainer}>
       <div className={styles.formDelete}>
-       <div className={`${styles.formGroup} ${styles.accountDetails}`}>
-         <div className={styles.formHeader}>
-           <h2 className={styles.containerTitle}>
-             Account
-           </h2>
-           <h2 className={styles.editButton}> <ChangeEmail userData={userData} changeProfileEmail={displayNewInfo}/>           
-            </h2> 
-         </div>
-         <div className={styles.formFields}>
-           <div>
+        <div className={`${styles.formGroup} ${styles.accountDetails}`}>
+          <div className={styles.formHeader}>
+            <h2 className={styles.containerTitle}>Account</h2>
+            <h2 className={styles.editButton}>
+              <ChangeEmail
+                userData={userData}
+                changeProfileEmail={displayNewInfo}
+              />
+            </h2>
+          </div>
+          <div className={styles.formFields}>
+            <div>
               <div className={styles.fieldTitle}>Email Address</div>
-                {userData ? userData?.email : <div>Loading...</div>}
-              </div>
+              {userData ? userData?.email : <div>Loading...</div>}
             </div>
-          
-         </div>  
-         <div>
-            <DeleteConfirmation closeFromChild={closeFromChild} userData={userData} isSelf={true}></DeleteConfirmation>
-         </div>
-
+          </div>
         </div>
+        <div>
+          <DeleteConfirmation
+            closeFromChild={closeFromChild}
+            userData={userData}
+            isSelf={true}
+          ></DeleteConfirmation>
+        </div>
+      </div>
 
-       
-       <div className={`${styles.formGroup} ${styles.personalDetails}`}>
-         <div className={styles.formHeader}>
-           <h2 className={styles.containerTitle}>
-             Personal
-           </h2>
-            <h2 className={styles.editButton}> <EditProfile userData={userData} displayNewInfo={displayNewInfo}/>           
-            </h2> 
-         </div>
-         <div className={styles.formFields}>
-           <div>
-              <div className={styles.fieldTitle}>First Name</div>
-                {userData ? userData.firstName : <div>Loading...</div>}
+      <div className={`${styles.formGroup} ${styles.personalDetails}`}>
+        <div className={styles.formHeader}>
+          <h2 className={styles.containerTitle}>Personal</h2>
+          <h2 className={styles.editButton}>
+            {' '}
+            <EditProfile userData={userData} displayNewInfo={displayNewInfo} />
+          </h2>
+        </div>
+        <div className={styles.formFields}>
+          <div>
+            <div className={styles.fieldTitle}>First Name</div>
+            {userData ? userData.firstName : <div>Loading...</div>}
 
             <div className={styles.fieldTitle}>Last Name</div>
-              {userData ? userData.lastName : <div>Loading...</div>}            
-           </div>
-           <div>
-             <div className={styles.fieldTitle}>Phone Number</div>
-                {userData ? userData.phoneNumber : <div>Loading...</div>}
-              <div className={styles.fieldTitle}>Zipcode</div>
-                {userData ? userData.zipcode : <div>Loading...</div>}                   
-           </div>
-           <div>
-            <div className={styles.fieldTitle}>Receive Newsletter</div>
-              <span className={userData?.receiveNewsletter ? 'yes' : 'no'}>
-                { userData ?(userData?.receiveNewsletter ? 'Yes' : 'No') : <div>Loading...</div>}
-              </span>
+            {userData ? userData.lastName : <div>Loading...</div>}
           </div>
-         </div>
-       </div>    
-    </div> 
-  )
-};
-
-
+          <div>
+            <div className={styles.fieldTitle}>Phone Number</div>
+            {userData ? userData.phoneNumber : <div>Loading...</div>}
+            <div className={styles.fieldTitle}>Zipcode</div>
+            {userData ? userData.zipcode : <div>Loading...</div>}
+          </div>
+          <div>
+            <div className={styles.fieldTitle}>Receive Newsletter</div>
+            <span className={userData?.receiveNewsletter ? 'yes' : 'no'}>
+              {userData ? (
+                userData?.receiveNewsletter ? (
+                  'Yes'
+                ) : (
+                  'No'
+                )
+              ) : (
+                <div>Loading...</div>
+              )}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
