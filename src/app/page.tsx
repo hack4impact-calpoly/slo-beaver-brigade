3a022d465231e2f50699336cec3ae7e85abe9865
Brady Welsh <blwelsh@calpoly.edu>
2025-04-14 18:01:35 -0700

    fix: home page edit link should now work correctly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -809,6 +809,7 @@ const handleButtonClickToStopPropogation = (event: React.MouseEvent<HTMLButtonEl
      </div>
      <ExpandedViewComponent
         eventDetails={eventForExpandedViewComponent}
+        editUrl={eventForExpandedViewComponent?._id}
         showModal={isExpandedViewComponentOpen}
         setShowModal={toggleExpandedViewComponentOpen}
         mutate={mutate}
