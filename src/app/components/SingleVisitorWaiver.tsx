dae17e01da0757192102671402f07b6648e4a1bb
Kayla Tran <kaylatran2021@gmail.com>
2025-05-12 16:43:15 -0700

    feat: waiver pop up styling allignment

--- src/app/components/SingleVisitorWaiver.tsx
+++ src/app/components/SingleVisitorWaiver.tsx
@@ -6,9 +6,12 @@ import {
   ModalHeader,
   ModalCloseButton,
   ModalBody,
-  Text
+  Text,
+  Flex,
+  Box
 } from "@chakra-ui/react";
 import { ISignedWaiver } from "database/signedWaiverSchema";
+import styles from "../styles/admin/editEvent.module.css";
 
 interface SignedWaiverModalProps {
   isOpen: boolean;
@@ -26,17 +29,45 @@ export default function SignedWaiverModal({
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Signed Waiver</ModalHeader>
+      <ModalContent
+        style={{ width: "60vw", height: "40vh", overflow: "auto" }}
+        maxW="100rem"
+      >
+        <ModalHeader
+          style={{
+            padding: "1% 5%",
+            textAlign: "left",
+            fontSize: "35px",
+            fontWeight: "bold",
+            fontFamily: "Lato",
+            width: "100%",
+          }}
+        >
+          {waiver.signeeName}
+        </ModalHeader>
         <ModalCloseButton />
-        <ModalBody>
-          <Text>
-            <strong>Name:</strong> {waiver.signeeName}
-          </Text>
-          <Text>
-            <strong>Date Signed:</strong>{" "}
-            {new Date(waiver.dateSigned).toLocaleDateString()}
-          </Text>
+        <hr />
+        <ModalBody
+          style={{ display: "flex", flexDirection: "column", padding: "0%" }}
+          className={styles.parentContainer}
+        >
+          <Box className={styles.infoBox}>
+            <Text className={styles.visitorInfoSmallHeader}>
+              Waiver Details
+            </Text>
+            <div className={styles.accountInfo} style={{ display: "flex" }}>
+              <div style={{ width: "100%" }}>
+                <Text className={styles.fieldInfo}>
+                  <Text as="span" className={styles.boldText}>Name</Text><br />
+                  {waiver.signeeName}
+                </Text>
+                <Text className={styles.fieldInfo}>
+                  <Text as="span" className={styles.boldText}>Date Signed</Text><br />
+                  {new Date(waiver.dateSigned).toLocaleDateString()}
+                </Text>
+              </div>
+            </div>
+          </Box>
         </ModalBody>
       </ModalContent>
     </Modal>
